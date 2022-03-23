0b2197df3af0e8b07ee3e73c1f45dc4e70621b7d
Seunghoon Yang <hyuio0228@gmail.com>
2022-03-24 01:40:23 +0900

    [Gold V] Title: 공유기 설치, Time: 340 ms, Memory: 38588 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -5,28 +5,25 @@ def solution(n: int, c: int) -> int:
     xs = [int(input()) for _ in range(n)]
     xs.sort()
 
-    start, end = 1, xs[-1]
+    start, end = 1, xs[-1]//(c-1) + 1
     minys = list()
 
     while start <= end:
         cnt = 1
-        miny = xs[-1]
         cx = xs[0]
         mid = (start + end) // 2
 
         for i in range(1, n):
             if xs[i] >= cx + mid:
                 cnt += 1
-                miny = min(xs[i] - cx, miny)
                 cx = xs[i]
 
         if cnt >= c:
             start = mid + 1
-            minys.append(miny)
         else:
             end = mid - 1
 
-    return max(minys)
+    return end
 
 
 print(solution(*map(int, input().split())))
