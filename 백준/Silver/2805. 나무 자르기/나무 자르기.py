91741ebd86e92fe01111356d1fcc40ac739853bd
Seunghoon Yang <hyuio0228@gmail.com>
2022-03-23 17:58:18 +0900

    [Silver III] Title: 나무 자르기, Time: 480 ms, Memory: 144088 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,17 +1,25 @@
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 def solution(n: int, m: int) -> int:
     trees = [*map(int, input().split())]
-    bottom, top = 0, max(trees)
-    while bottom <= top:
-        mid = (bottom + top) // 2
-        cut_off = sum(tree - mid if tree > mid else 0 for tree in trees)
-        if cut_off >= m:
-            bottom = mid + 1
+    counter = Counter(trees)
+    trees = sorted(set(trees), reverse=True)
+    trees.append(0)
+    res = trees[0]
+    width = 0
+
+    for i in range(len(trees) - 1):
+        width += counter[trees[i]]
+        if (gap := trees[i] - trees[i + 1]) * width >= m:
+            res -= m // width + (m % width != 0)
+            break
         else:
-            top = mid - 1
-    return bottom - 1
+            m -= gap * width
+            res -= gap
+    
+    return res
 
 
 print(solution(*map(int, input().split())))
