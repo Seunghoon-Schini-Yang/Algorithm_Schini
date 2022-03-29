e03726a312d6aca0a004c96206ce84a6d3470aba
Seunghoon Yang <hyuio0228@gmail.com>
2022-03-29 17:31:42 +0900

    [Gold III] Title: 파일 합치기, Time: 2032 ms, Memory: 137056 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -6,21 +6,16 @@ def solution(n: int) -> int:
     files = [0] + list(map(int, input().split()))
     acc = list(accumulate(files))
     dp = [[0] * (n+1) for _ in range(n+1)]
+    
+    for step in range(1, n):
+        for e in range(step+1, n+1):
+            s = e-step
+            if e == s+1:
+                dp[s][e] = acc[e] - acc[s-1]
+            else:
+                dp[s][e] = acc[e] - acc[s-1] + min(dp[s][k] + dp[k+1][e] for k in range(s, e))
 
-    return partial_sum(1, n, acc, dp) - acc[n]
-
-
-def partial_sum(s: int, e: int, acc: list, dp: list) -> int:
-    if dp[s][e]:
-        return dp[s][e]
-
-    range_sum = acc[e] - acc[s-1]
-    if s == e:
-        dp[s][e] = range_sum
-        return dp[s][e]
-
-    dp[s][e] = range_sum + min(partial_sum(s, k, acc, dp) + partial_sum(k+1, e, acc, dp) for k in range(s, e))
-    return dp[s][e]
+    return dp[1][n]
 
 
 print('\n'.join([str(solution(int(input()))) for _ in range(int(input()))]))
