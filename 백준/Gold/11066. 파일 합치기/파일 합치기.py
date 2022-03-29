0696c172e51ad6885a7fe92bcb048a27a3c62240
Seunghoon Yang <hyuio0228@gmail.com>
2022-03-29 17:34:18 +0900

    [Gold III] Title: 파일 합치기, Time: 948 ms, Memory: 38880 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -6,14 +6,23 @@ def solution(n: int) -> int:
     files = [0] + list(map(int, input().split()))
     acc = list(accumulate(files))
     dp = [[0] * (n+1) for _ in range(n+1)]
+    knuth = [[0] * (n+1) for _ in range(n+1)]
     
     for step in range(1, n):
         for e in range(step+1, n+1):
             s = e-step
             if e == s+1:
                 dp[s][e] = acc[e] - acc[s-1]
+                knuth[s][e] = s
             else:
-                dp[s][e] = acc[e] - acc[s-1] + min(dp[s][k] + dp[k+1][e] for k in range(s, e))
+                miny = sys.maxsize
+                for k in range(knuth[s][e-1], knuth[s+1][e] + 1):
+                    if dp[s][k] + dp[k+1][e] < miny:
+                        miny = dp[s][k] + dp[k+1][e]
+                        min_k = k
+                knuth[s][e] = min_k
+                dp[s][e] = acc[e] - acc[s-1] + miny
+
 
     return dp[1][n]
 
