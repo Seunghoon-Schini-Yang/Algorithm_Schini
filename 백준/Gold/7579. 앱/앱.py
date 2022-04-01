4e88d663058822508b5eed0b2420377889a1f33a
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-01 18:45:09 +0900

    [Gold III] Title: 앱, Time: 136 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,25 +1,23 @@
 import sys
 input = sys.stdin.readline
-from collections import defaultdict
 
 def sol(n: int, m: int) -> int:
     memos = map(int, input().split())
     costs = map(int, input().split())
-    dp = defaultdict(int)
-    dp[0] = 0
+    dp = dict({0: 0})
     for cost, memo in zip(costs, memos):
         temp = dict()
         for ps_c in dp:
             if dp[ps_c] >= m:
                 continue
             idx = ps_c+cost
-            if idx not in dp or dp[idx] < dp[ps_c]+memo:
+            if dp.get(idx, 0) < dp[ps_c]+memo:
                 temp[idx] = dp[ps_c]+memo
         for k, v in temp.items():
             dp[k] = v
 
     for i in range(10001):
-        if dp[i] >= m:
+        if dp.get(i, 0) >= m:
             return i
 
 print(sol(*map(int, input().split())))
