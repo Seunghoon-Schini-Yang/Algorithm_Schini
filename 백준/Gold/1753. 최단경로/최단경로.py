99f65560abb1c0661752156c08d2b6aef98960a9
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-07 01:22:13 +0900

    [Gold V] Title: 최단경로, Time: 1092 ms, Memory: 93768 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -3,15 +3,16 @@ input = sys.stdin.readline
 from heapq import heappop, heappush, heapify
 
 def sol(v: int, e: int, s: int) -> str:
-    INF = sys.maxsize
-    shortest = [INF]*(v+1)
+    maxy = 3_000_001
+    shortest = [maxy]*(v+1)
+    shortest[s] = 0
     graph = [[] for _ in range(v+1)]
 
     for _ in range(e):
         n_1, n_2, w  = map(int, input().split())
         graph[n_1].append((w,n_2))
 
-    heap = [(0,s)]
+    heap = graph[s][:]
     heapify(heap)
 
     while heap:
@@ -21,7 +22,7 @@ def sol(v: int, e: int, s: int) -> str:
             for c_w, c_n in graph[n]:
                 heappush(heap, (w+c_w,c_n))
 
-    return '\n'.join(str(dist) if dist<INF else 'INF' for dist in shortest[1:])
+    return '\n'.join(str(dist) if dist<maxy else 'INF' for dist in shortest[1:])
 
 
 print(sol(*map(int, input().split()), int(input())))
