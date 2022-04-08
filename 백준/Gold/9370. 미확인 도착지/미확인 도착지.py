c9a9cc066da536eb90035983c82acd76d31b1978
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-08 22:49:25 +0900

    [Gold II] Title: 미확인 도착지, Time: 256 ms, Memory: 42640 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -5,59 +5,33 @@ from heapq import heappop, heappush
 def sol(nodes: int, edges: int, e_nodes: int) -> str:
     INF = sys.maxsize
     s_node, s_used, e_used = map(int, input().split())
-    graph = [[] for _ in range(nodes+1)]
+    graph = [{} for _ in range(nodes+1)]
     for _ in range(edges):
         a,b,d = map(int, input().split())
-        graph[a].append((d,b))
-        graph[b].append((d,a))
+        graph[a][b] = d*2
+        graph[b][a] = d*2
 
-    for c_w,c_n in graph[s_used]:
-        if c_n==e_used:
-            used_w = c_w
-            break
+    graph[s_used][e_used] -= 1
+    graph[e_used][s_used] -= 1
 
-    entire, from_s, from_e = [INF]*(nodes+1), [INF]*(nodes+1), [INF]*(nodes+1)
-    entire[s_node] = from_s[s_used] = from_e[e_used] = 0
+    entire = [INF]*(nodes+1)
+    entire[s_node] = 0
 
     q = list()
-    heappush(q,(0,s_node))
+    heappush(q, (0,s_node))
     while q:
         p_w,p_n = heappop(q)
         if entire[p_n] < p_w:
             continue
-        for c_w,c_n in graph[p_n]:
-            if entire[p_n]+c_w < entire[c_n]:
-                entire[c_n] = entire[p_n]+c_w
+        for c_n,c_w in graph[p_n].items():
+            if p_w+c_w < entire[c_n]:
+                entire[c_n] = p_w+c_w
                 heappush(q, (entire[c_n],c_n))
-
-    heappush(q,(0,s_used))
-    while q:
-        p_w,p_n = heappop(q)
-        if from_s[p_n] < p_w:
-            continue
-        for c_w,c_n in graph[p_n]:
-            if c_n==s_node or c_n==e_used:
-                continue
-            if from_s[p_n]+c_w < from_s[c_n]:
-                from_s[c_n] = from_s[p_n]+c_w
-                heappush(q, (from_s[c_n],c_n))
-
-    heappush(q,(0,e_used))
-    while q:
-        p_w,p_n = heappop(q)
-        if from_e[p_n] < p_w:
-            continue
-        for c_w,c_n in graph[p_n]:
-            if c_n==s_node or c_n==s_used:
-                continue
-            if from_e[p_n]+c_w < from_e[c_n]:
-                from_e[c_n] = from_e[p_n]+c_w
-                heappush(q, (from_e[c_n],c_n))
     
     res = []
     for _ in range(e_nodes):
         k = int(input())
-        if used_w + min(entire[s_used]+from_e[k], entire[e_used]+from_s[k]) <= entire[k]:
+        if entire[k]<INF and entire[k]&1:
             res.append(k)
     res.sort()
 
