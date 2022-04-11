10e6c6ff00fbfc2edb400114488b0e9f4bfb63e3
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-11 15:42:20 +0900

    [Gold V] Title: 최단경로, Time: 732 ms, Memory: 72024 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,28 +1,31 @@
 import sys
 input = sys.stdin.readline
-from heapq import heappop, heappush, heapify
+from heapq import heappop, heappush
+import math
 
 def sol(v: int, e: int, s: int) -> str:
-    maxy = 3_000_001
-    shortest = [maxy]*(v+1)
+    INF = math.inf
+    is_inf = math.isinf
+    shortest = [INF]*(v+1)
     shortest[s] = 0
     graph = [[] for _ in range(v+1)]
+    visited = set()
 
     for _ in range(e):
-        n_1, n_2, w  = map(int, input().split())
+        n_1,n_2,w  = map(int, input().split())
         graph[n_1].append((w,n_2))
 
-    heap = graph[s][:]
-    heapify(heap)
+    heap = [(0,s)]
 
     while heap:
-        w, n = heappop(heap)
-        if w < shortest[n]:
-            shortest[n] = w
-            for c_w, c_n in graph[n]:
-                heappush(heap, (w+c_w,c_n))
-
-    return '\n'.join(str(dist) if dist<maxy else 'INF' for dist in shortest[1:])
+        w,n = heappop(heap)
+        visited.add(n)
+        for c_w,c_n in graph[n]:
+            if w+c_w < shortest[c_n]:
+                shortest[c_n] = w+c_w
+                heappush(heap, (shortest[c_n],c_n))
+                
+    return '\n'.join('INF' if is_inf(dist) else str(dist) for dist in shortest[1:])
 
 
 print(sol(*map(int, input().split()), int(input())))
