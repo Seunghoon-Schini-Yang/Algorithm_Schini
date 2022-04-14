d3b75eadb370f1509a834bee9111460ee2a25e90
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-14 14:15:12 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 44428 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+import math
+
+def sol(n: int) -> None:
+    nums = sorted(map(int, input().split()))
+    miny = math.inf
+    i = x = 0
+    j = y = len(nums)-1
+
+    while i<j:
+        sum_ij = nums[i]+nums[j]
+        if abs(sum_ij) < miny:
+            miny = abs(sum_ij)
+            x = i; y = j
+        if sum_ij > 0:
+            j -= 1
+        elif sum_ij < 0:
+            i += 1
+        else:
+            break
+    print(nums[x],nums[y])
+
+
+sol(int(input()))
