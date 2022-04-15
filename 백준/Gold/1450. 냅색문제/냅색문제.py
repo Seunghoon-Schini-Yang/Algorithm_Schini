5e7a08d0096248962774aabab785d02df1011cfb
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-15 16:22:09 +0900

    [Gold I] Title: 냅색문제, Time: 212 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.py
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -1,3 +1,5 @@
+# meet in the middle, binary search
+# O(N * 2^(N/2))
 import sys
 input = sys.stdin.readline
 from itertools import product
@@ -8,10 +10,8 @@ def sol(n: int, c: int) -> int:
     fst_h = list(next(nums) for _ in range(n>>1))
     snd_h = list(next(nums) for _ in range(n-(n>>1)))
 
-    fst_sub_sum = [sum(fst_h[i] if comb[i] else 0 for i in range(n>>1)) for comb in product(*((0,1) for _ in range(n>>1)))]
+    fst_sub_sum = sorted(sum(fst_h[i] if comb[i] else 0 for i in range(n>>1)) for comb in product(*((0,1) for _ in range(n>>1))))
     snd_sub_sum = [sum(snd_h[i] if comb[i] else 0 for i in range(n-(n>>1))) for comb in product(*((0,1) for _ in range(n-(n>>1))))]
-
-    fst_sub_sum.sort()
 
     return sum(bisect_right(fst_sub_sum, c-v) for v in snd_sub_sum)
 
