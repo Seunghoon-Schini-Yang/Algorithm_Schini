db0af30b13613637e8e897ddafb2b9325641e763
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-16 17:04:21 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 1144 ms, Memory: 128736 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,23 +1,19 @@
-# teferi00 님 코드 참고
-# dynamic programming + zip iterator
 import sys
 input = sys.stdin.readline
 
-def sol(n: int) -> int:
-    size_dp = [0]*(n+1)
-    val_dp = [[0]*n for _ in range(n)]
-    for i in range(n-1):
-        size_dp[i] = next(map(int, input().split()))
-    size_dp[n-1] ,size_dp[n] = map(int, input().split())
+def solution(n: int) -> int:
+    h = [0] * n
+    w = [0] * n
+    for i in range(n):
+        h[i], w[i] = map(int, input().split())
 
-    for gap in range(1,n):
-        for i in range(n-gap):
-            j = i+gap
-            size_ij = size_dp[i]*size_dp[j+1]
-            # val_dp[i][j] = min(size_ij*size_dp[k+1] + val_dp[i][k] + val_dp[k+1][j] for k in range(i,j))
-            val_dp[i][j] = val_dp[j][i] = min(size_ij*size_k + front + back for size_k,front,back in zip(size_dp[i+1:j+1],val_dp[i][i:j],val_dp[j][i+1:j+1]))
+    dp = [[0] * n for _ in range(n)]
+    for step in range(1, n):
+        for r in range(n-step):
+            m = r + step
+            dp[r][m] = min(h[r]*w[k]*w[m] + dp[r][k] + dp[k+1][m] for k in range(r, m))
 
-    return val_dp[0][n-1]
+    return dp[0][n-1]
 
 
-print(sol(int(input())))
+print(solution(int(input())))
