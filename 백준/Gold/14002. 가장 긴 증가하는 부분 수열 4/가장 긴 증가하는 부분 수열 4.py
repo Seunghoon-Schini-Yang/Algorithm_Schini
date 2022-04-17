e5725011bfb029ba8969f046395af9eb3c2cd88c
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-18 00:51:00 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 72 ms, Memory: 32904 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,44 +1,39 @@
-# dynamic programming
-# bisect 안써서 O(n^2) 일듯 (level 순차 탐색)
+# binary search + dynamic programming
+# redpigeon 님 코드 참고
 import sys
 input = sys.stdin.readline
 from bisect import bisect_left
 
 def sol(n: int) -> str:
     seq = list(map(int, input().split()))
-    dp = [[[0,seq[0]]]]
+    track = [0]*n
     lis = [seq[0]]
-    level = 0
     lis_len = 1
 
-    for v in seq[1:]:
-        cur_idx = bisect_left(lis, v)
+    for i in range(1,n):
+        v = seq[i]
+        cur_idx = bisect_left(lis,v)
+        track[i] = cur_idx
         if cur_idx == lis_len:
             lis.append(v)
-            dp.append([[level,v]])
             lis_len += 1
-        elif lis[cur_idx] == v:
-            continue
         else:
+            if lis[cur_idx] == v:
+                continue
             lis[cur_idx] = v
-            level += 1
-            dp[cur_idx].append([level,v])
 
-    level = dp[-1][-1][0]
-    ans = [0]*lis_len
-    ans[-1] = str(dp[-1][-1][1])
+    print(lis_len)
 
-    for i in range(lis_len-2,-1,-1):
-        cur_len = len(dp[i])
-        j = 0
-        while j < cur_len and dp[i][j][0] <= level:
-            temp_level, temp_val = dp[i][j]
-            j += 1
-        ans[i] = str(temp_val)
-        if temp_level < level:
-            level = temp_level
+    path = [0]*lis_len
+    lis_len -= 1
+    for i in range(n-1,-1,-1):
+        if lis_len < 0:
+            break
+        if track[i] == lis_len:
+            path[lis_len] = str(seq[i])
+            lis_len -= 1
 
-    return str(lis_len) + '\n' + ' '.join(ans)
+    print(' '.join(path))
 
 
-print(sol(int(input())))
+sol(int(input()))
