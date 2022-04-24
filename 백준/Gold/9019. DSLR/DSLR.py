99e8e6fc52425ffc7770fb834231da0f0e3d697f
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-24 22:22:43 +0900

    [Gold V] Title: DSLR, Time: 8400 ms, Memory: 216232 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def sol(a: int, b: int) -> str:
+    dp = [[-1]*10000 for _ in range(2)]
+    dp[0][a] = -2
+    q = deque([a])
+
+    while q:
+        for _ in range(len(q)):
+            cur = q.popleft()
+
+            if cur == b:
+                ans = ''
+                while cur != a:
+                    ans = dp[1][cur] + ans
+                    cur = dp[0][cur]
+                return ans
+            
+            D = (cur*2) % 10000
+            S = cur-1 if cur else 9999
+            str_cur = str(cur).rjust(4,'0')
+            L = int(str_cur[1:]+str_cur[0])
+            R = int(str_cur[-1]+str_cur[:-1])
+            for x,char in (D,'D'),(S,'S'),(L,'L'),(R,'R'):
+                if dp[0][x] == -1:
+                    dp[0][x] = cur
+                    dp[1][x] = char
+                    q.append(x)
+
+
+print('\n'.join(sol(*map(int, input().split())) for _ in range(int(input()))))
