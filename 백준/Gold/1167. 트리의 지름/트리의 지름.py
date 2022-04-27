4782606efc58af9beb4183d0a05186dc89f9d214
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-27 16:26:26 +0900

    [Gold III] Title: 트리의 지름, Time: 480 ms, Memory: 70344 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,42 +1,32 @@
-# my
+# dfs + recursive
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(pow(10,9))
 
-def sol(v: int) -> int:
-    maxy = -sys.maxsize
-    tree = [{} for _ in range(v+1)]
-    dp = [[] for _ in range(v+1)]
-    q = []
-    for _ in range(v):
+def sol(n: int) -> int:
+    def dfs(v: int, p_v: int):
+        maxy = maxy_v = 0
+        if v != p_v and len(tree[v]) == 1:
+            return v, 0
+        
+        for key,val in tree[v].items():
+            if key != p_v:
+                leaf, acc = dfs(key, v)
+                if acc+val > maxy:
+                    maxy = acc+val
+                    maxy_v = leaf
+
+        return maxy_v, maxy
+
+    tree = [{} for _ in range(n+1)]
+    for _ in range(n):
         line = map(int, input().split())
         p_n = next(line)
-        cnt = 0
         while (c_n := next(line)) != -1:
             tree[p_n][c_n] = next(line)
-            cnt += 1
-        if cnt == 1:
-            q.append(p_n)
-
-    for _ in range(v-1):
-        p_n = q.pop()
-        c_n,_ = tree[p_n].popitem()
-        w = tree[c_n].pop(p_n)
-
-        if not tree[c_n]:
-            return max(maxy,w)
-        
-        dp[c_n].append(w)
-        if len(tree[c_n]) == 1:
-            if len(dp[c_n]) > 1:
-                temp = sum(sorted(dp[c_n])[-2:])
-                if maxy < temp:
-                    maxy = temp
-            for key in tree[c_n]:
-                cc_n = key
-            temp = max(dp[c_n])
-            tree[cc_n][c_n] += temp
-            tree[c_n][cc_n] += temp
-            q.append(c_n)
+    
+    v1 = dfs(1, 1)[0]
+    return dfs(v1, v1)[1]
 
 
 print(sol(int(input())))
