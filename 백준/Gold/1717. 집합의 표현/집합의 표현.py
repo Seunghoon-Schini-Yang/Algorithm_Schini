76353086ef47a30e66545080d0c791156787cbfd
Seunghoon Yang <hyuio0228@gmail.com>
2022-04-28 16:23:35 +0900

    [Gold IV] Title: 집합의 표현, Time: 320 ms, Memory: 78820 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,38 +1,36 @@
 # my
-# disjoint set
+# disjoint set + path compression + recursive + union by rank
 import sys
 input = sys.stdin.readline
 
 def sol(n: int, m: int) -> str:
     def find(x: int) -> int:
-        temp = []
-        while x != root[x]:
-            temp.append(x)
-            x = root[x]
-        
-        # path compression
-        for v in temp:
-            root[v] = x
-        
-        return x
+        if x != parent[x]:
+            parent[x] = find(parent[x])
+        return parent[x]
 
     
-    root = [i for i in range(n+1)]
+    def union(x: int, y: int):
+        if rank[x] > rank[y]:
+            parent[y] = x
+        else:
+            parent[x] = y
+            if rank[x] == rank[y]:
+                rank[y] += 1
+
+    
+    rank = [0] * (n+1)
+    parent = [i for i in range(n+1)]
     ans = list()
     for _ in range(m):
         op,a,b = map(int, input().split())
         x,y = find(a),find(b)
         if op:
-            if x == y:
-                ans.append('YES')
-            else:
-                ans.append('NO')
+            ans.append('YES' if x==y else 'NO')
         else:
-            if x == y:
-                continue
-            else:
-                root[y] = x
-
+            if x != y:
+                union(x, y)
+    
     return '\n'.join(ans)
 
 
