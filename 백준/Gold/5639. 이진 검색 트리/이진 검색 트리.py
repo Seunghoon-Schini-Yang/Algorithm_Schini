62d4b52e14d155984bcb5bb13e31256fa7d96145
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-01 12:47:08 +0900

    [Gold V] Title: 이진 검색 트리, Time: 80 ms, Memory: 41776 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,3 +1,5 @@
+# eric9709 님 코드 참고
+# recursive + binary search
 import sys
 input = sys.stdin.readlines
 sys.setrecursionlimit(pow(10,9))
@@ -7,23 +9,34 @@ def sol(lines: str) -> list:
         if s > e:
             return
         
-        root = pre[s]
-        larger = e+1
-        for i in range(s+1,e+1):
-            if pre[i] > root:
-                larger = i
-                break
-        
-        get_post(s+1, larger-1)
-        get_post(larger, e)
-        post.append(root)
+        root = preodr[s]
+        if s == e:
+            postodr.append(root)
+            return
+
+        if root < preodr[s+1] or root > preodr[e]:
+            get_post(s+1, e)
+            postodr.append(root)
+            return
+
+        s += 1
+        c_s = s; c_e = e
+        while c_s+1 < c_e:
+            m = (c_s+c_e)//2
+            if preodr[m] > root:
+                c_e = m
+            else:
+                c_s = m
+            
+        get_post(s, c_s)
+        get_post(c_e, e)
+        postodr.append(root)
 
 
-    pre = list(map(int, lines))
-    n = len(pre)
-    post = []
-    get_post(0, n-1)
-    return post
+    postodr = []
+    preodr = list(map(int, lines))
+    get_post(0, len(preodr)-1)
+    return postodr
 
 
 print(*sol(input()))
