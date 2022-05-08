e68600a4d5afdb8b6b3651999459c66d715814b7
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-08 17:13:51 +0900

    [Gold V] Title: 트리와 쿼리, Time: 448 ms, Memory: 158216 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -0,0 +1,31 @@
+# my
+# dp + recursive
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(pow(10, 9))
+
+
+def sol(n: int, r: int, q: int) -> str:
+    def make_tree(v: int, p: int):
+        c_sum = 0
+        for c_v in tree[v]:
+            if c_v != p:
+                c_sum += 1
+                c_sum += make_tree(c_v, v)
+
+        subtree_dp[v] = c_sum+1
+        return c_sum
+
+    
+    tree = [[] for _ in range(n+1)]
+    subtree_dp = [0] * (n+1)
+    for _ in range(n-1):
+        v1,v2 = map(int, input().split())
+        tree[v1].append(v2)
+        tree[v2].append(v1)
+    
+    make_tree(r, -1)
+    return '\n'.join(str(subtree_dp[int(input())]) for _ in range(q))
+
+
+print(sol(*map(int, input().split())))
