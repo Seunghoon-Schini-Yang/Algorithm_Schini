9fe79e5a0c7b10ae928343628bdd147e6ae2e60d
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-09 20:33:12 +0900

    [Silver III] Title: 대칭 차집합, Time: 220 ms, Memory: 88248 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -6,10 +6,10 @@ def sol() -> int:
     a_org = set(input().split())
     a = a_org.copy()
     b = set(input().split())
+    a_rm = a.remove; b_rm = b.remove
     for x in a_org:
         if x in b:
-            a.remove(x)
-            b.remove(x)
+            a_rm(x); b_rm(x)
 
     return len(a) + len(b)
 
