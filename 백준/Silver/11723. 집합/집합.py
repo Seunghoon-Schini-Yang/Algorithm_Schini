5bb4a502c7885d542fb68a4ed2888b5f546d19d9
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-10 18:24:51 +0900

    [Silver V] Title: 집합, Time: 3340 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -4,25 +4,25 @@ input = sys.stdin.readline
 
 def sol(n: int) -> None:
     s = set()
-    s_rm = s.discard; s_add = s.add
-    s_clr = s.clear; s_update = s.update
+    # s_rm = s.discard; s_add = s.add
+    # s_clr = s.clear; s_update = s.update
     for _ in range(n):
         query = input().split()
         
         if query[0][0] == 'a':
             if query[0][1] == 'd':
-                s_add(int(query[1]))
+                s.add(int(query[1]))
             else:
-                s_clr(); s_update(range(1, 21))
+                s.clear(); s.update(range(1, 21))
         elif query[0][0] == 'r':
-            s_rm(int(query[1]))
+            s.discard(int(query[1]))
         elif query[0][0] == 'c':
             print(1 if int(query[1]) in s else 0)
         elif query[0][0] == 't':
             x = int(query[1])
-            s_rm(x) if x in s else s_add(x)
+            s.discard(x) if x in s else s.add(x)
         else:
-            s_clr()
+            s.clear()
 
 
 sol(int(input()))
