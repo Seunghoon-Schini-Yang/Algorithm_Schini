0e6fa06ea2e5d07701847758139276d48b2b636f
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-11 12:24:59 +0900

    [Silver III] Title: 어린 왕자, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,5 +1,4 @@
 import sys
-from math import sqrt
 input = sys.stdin.readline
 print = sys.stdout.write
 
@@ -8,7 +7,7 @@ def sol(xs: int, ys: int, xe: int, ye: int) -> str:
     cnt = 0
     for _ in range(int(input())):
         cx, cy, r = map(int, input().split())
-        cnt += (r > sqrt((cx-xs)**2 + (cy-ys)**2)) ^ (r > sqrt((cx-xe)**2 + (cy-ye)**2))
+        cnt += (r**2 > (cx-xs)**2 + (cy-ys)**2) ^ (r**2 > (cx-xe)**2 + (cy-ye)**2)
     return str(cnt) + '\n'
 
 for _ in range(int(input())):
