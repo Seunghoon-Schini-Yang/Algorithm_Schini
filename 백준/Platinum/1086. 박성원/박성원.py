ccde55635fb155fd4f7a9c7594d4a5ed34bfa56d
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-16 12:28:33 +0900

    [Platinum V] Title: 박성원, Time: 616 ms, Memory: 143772 KB -BaekjoonHub

--- 백준/Platinum/1086. 박성원/박성원.py
+++ 백준/Platinum/1086. 박성원/박성원.py
@@ -1,23 +1,8 @@
-import sys, math
+import sys
 input = sys.stdin.readline
 
 
 def sol(n: int) -> str:
-    def dfs(mask: int, n_len: int) -> None:
-        if not n_len or visited[mask]:
-            return
-
-        visited[mask] = 1
-        for i in range(n):
-            if not mask&(1<<i):
-                rem = modulo(arr[i].ljust(n_len, '0'))
-                c_mask = mask|(1<<i)
-                dfs(c_mask, n_len-len(arr[i]))
-                for j in range(k):
-                    if memo[c_mask][j]:
-                        memo[mask][(rem+j)%k] += memo[c_mask][j]
-
-
     def modulo(n: str) -> int:
         n_len = len(n)
         while n_len >= 3:
@@ -28,32 +13,34 @@ def sol(n: int) -> str:
         if n >= k:
             n %= k
         return n
-    
+
 
     arr = [input().rstrip() for _ in range(n)]
-    tot_len = 0
-    for num in arr:
-        tot_len += len(num)
     k = int(input())
+    mod_arr = [(pow(10, len(elem), k), modulo(elem)) for elem in arr]
     memo = [[0] * k for _ in range(1<<n)]
-    memo[(1<<n)-1][0] = 1
-    visited = [0] * (1<<n)
+    memo[0][0] = 1
 
-    dfs(0, tot_len)
-    numer = memo[0][0]
-    denom = math.factorial(n)
+    for mask in range(1<<n):
+        for i in range(n):
+            if mask&(1<<i):
+                p_mask = mask & ~(1<<i)
+                for j in range(k):
+                    memo[mask][(j*mod_arr[i][0] + mod_arr[i][1]) % k] += memo[p_mask][j]
+
+    numer = memo[(1<<n)-1][0]
+    denom = sum(memo[(1<<n)-1])
     if not numer:
         return '0/1'
     else:
-        gcd = math.gcd(numer, denom)
+        gcd = get_gcd(numer, denom)
         return f'{numer//gcd}/{denom//gcd}'
 
-'''
+
 def get_gcd(x: int, y: int) -> int:
     if not y % x:
         return x
     return get_gcd(y % x, x)
-'''
 
 
 print(sol(int(input())))
