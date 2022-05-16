3cbf73696c1a2d458dc2a295bebc61cb5ef440a2
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-17 00:55:10 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,18 +1,14 @@
-import math
-
-
 def sol(a: int, b: int) -> str:
     if a > b:
         a,b = b,a
-    g = math.gcd(a,b)
+    g = gcd(a,b)
     return f'{g}\n{a*b//g}'
 
 
-'''
 def gcd(a: int, b: int) -> int:
     if not b%a:
         return a
-    return gcd(a, b%a)
-'''
+    return gcd(b%a, a)
+
 
 print(sol(*map(int, input().split())))
