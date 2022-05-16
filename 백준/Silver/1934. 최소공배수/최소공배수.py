b398a5faf5419ed3eb98e98c6a87d2a48bac8207
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-17 01:54:43 +0900

    [Silver V] Title: 최소공배수, Time: 76 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/1934. 최소공배수/최소공배수.py
+++ 백준/Silver/1934. 최소공배수/최소공배수.py
@@ -1,20 +1,10 @@
 import sys
+from math import lcm
 input = sys.stdin.readline
-print = sys.stdout.write
 
 
 def sol(n: int) -> None:
-    for _ in range(n):
-        a,b = map(int, input().split())
-        if a > b:
-            a,b = b,a
-        print(f'{a*b//gcd(a,b)}\n')
+    return '\n'.join(str(lcm(*map(int, input().split()))) for _ in range(n))
 
 
-def gcd(a: int, b: int) -> int:
-    if not a:
-        return b
-    return gcd(b%a, a)
-
-
-sol(int(input()))
+print(sol(int(input())))
