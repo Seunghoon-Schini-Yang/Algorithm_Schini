393cbd26ab2c52bc82fffca99f872386cc4e7330
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-17 14:57:23 +0900

    [Silver I] Title: 이항 계수 2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,21 +1,8 @@
-import sys
-sys.setrecursionlimit(pow(10, 9))
-
-
 def sol(n: int, k: int) -> int:
-    def comb(n: int, k: int) -> int:
-        if not k or n==k:
-            return 1
-        if memo[n][k]:
-            return memo[n][k]
-        
-        memo[n][k] = (comb(n-1,k-1) + comb(n-1,k)) % 10007
-        return memo[n][k]
-
-    
-    k = min(n-k, k)
-    memo = [[0]*(k+1) for _ in range(n+1)]
-    return comb(n,k)
+    k = min(n-k, k); ans = 1
+    for s,b in enumerate(range(n, n-k, -1), start=1):
+        ans = ans*b//s
+    return ans%10007
 
 
 print(sol(*map(int, input().split())))
