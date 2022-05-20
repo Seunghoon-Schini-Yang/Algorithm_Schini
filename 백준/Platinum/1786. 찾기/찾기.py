205f041076dc711f3ee351659a10ae0e324c61ad
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-20 14:20:58 +0900

    [Platinum V] Title: 찾기, Time: 604 ms, Memory: 60376 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -0,0 +1,47 @@
+def sol(t: str, p: str) -> None:
+    p_len = len(p); t_len = len(t)
+    if p_len > t_len:
+        print(0)
+        return
+    lps = [0] * p_len
+    cnt = 0; idx = list()
+
+    # find LPS in p
+    i = 1; j = 0
+    while i < p_len:
+        if p[i] == p[j]:
+            j += 1
+            i += 1
+        else:
+            if not j:
+                i += 1
+            else:
+                lps[i-1] = j
+                j = lps[j-1]
+    if j > 0:
+        lps[i-1] = j
+
+    # find p in t
+    i = j = 0
+    while i < t_len:
+        if j == p_len:
+            cnt += 1
+            idx.append(i-p_len+1)
+            j = lps[j-1]
+        elif p[j] != t[i]:
+            if not j:
+                i += 1
+            else:
+                j = lps[j-1]
+        else:
+            i += 1; j += 1
+
+    if j == p_len:
+        cnt += 1
+        idx.append(i-p_len+1)
+
+    print(len(idx))
+    print(*idx)
+
+
+sol(input(), input())
