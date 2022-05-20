3f90f1e2bbd2847260017d40dc503fc22bdc9df8
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-20 17:16:49 +0900

    [Platinum V] Title: 문자열 제곱, Time: 1512 ms, Memory: 42604 KB -BaekjoonHub

---
+++ 백준/Platinum/4354. 문자열 제곱/문자열 제곱.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+# print = sys.stdout.write
+
+
+def sol() -> None:
+    while True:
+        s = input().rstrip()
+        if s == '.':
+            return
+        s_len = len(s)
+        
+        quot,rem = divmod(s_len, s_len - lps(s, s_len))
+        print(1 if rem else quot)
+
+
+def lps(s: str, s_len: int) -> int:
+    s_len = len(s)
+    lps = [0] * s_len
+
+    j = 0; i = 1
+    while i < s_len:
+        if s[j] == s[i]:
+            i += 1; j += 1
+        else:
+            if not j:
+                i += 1
+            else:
+                lps[i-1] = j
+                j = lps[j-1]
+    
+    return j
+
+
+sol()
