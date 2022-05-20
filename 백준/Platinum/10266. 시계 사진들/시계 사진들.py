b76c41182aba6501d0e60a5b1ed20de26aacd8e2
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-21 03:18:12 +0900

    [Platinum IV] Title: 시계 사진들, Time: 424 ms, Memory: 57180 KB -BaekjoonHub

--- 백준/Platinum/10266. 시계 사진들/시계 사진들.py
+++ 백준/Platinum/10266. 시계 사진들/시계 사진들.py
@@ -4,56 +4,50 @@ input = sys.stdin.readline
 
 
 def sol(n: int) -> str:
-    def get_diff_arr(angles: str) -> list:
-        angles = sorted(map(int, angles.split()))
-        # diff = [0] * n
-        # for i in range(n-1):
-        #     diff[i] = angles[i+1] - angles[i]
-        # diff[-1] = angles[0] + 360_000 - angles[-1]
-        # return diff
-
-        # jnooree 님 코드 참고
-        return [x-y for x,y in zip(angles[1:] + [angles[0] + 360_000], angles)]
-
-
-    def failure_fuc() -> list:
-        lps = [0] * n
-        j = 0; i = 1
-
-        while i < n:
-            if p[j] == p[i]:
-                j += 1; i += 1
-            elif not j:
-                i += 1
-            else:
-                lps[i-1] = j
-                j = lps[j-1]
-        if j > 0:
-            lps[i-1] = j
-        
-        return lps
-
-    
-    def kmp() -> str:
-        j = i = 0
-        while i < 2*n - 1:
-            if p[j] == t[i]:
-                j += 1; i += 1
-                if j == n:
-                    return 'possible'
-            elif not j:
-                i += 1
-            else:
-                j = lps[j-1]
-
-        return 'impossible'
-
-    
     t = get_diff_arr(input())
     t = t + t[:-1]
     p = get_diff_arr(input())
-    lps = failure_fuc()
-    return kmp()
+    lps = failure_fuc(n, p)
+    return 'possible' if kmp(n, p, t, lps)  else 'impossible'
+
+
+def get_diff_arr(angles: str) -> list:
+    angles = sorted(map(int, angles.split()))
+    # jnooree 님 코드 참고
+    return [x-y for x,y in zip(angles[1:] + [angles[0] + 360_000], angles)]
+
+
+def failure_fuc(n: int, p: list) -> list:
+    lps = [0] * n
+    j = 0; i = 1
+
+    while i < n:
+        if p[j] == p[i]:
+            j += 1; i += 1
+        elif not j:
+            i += 1
+        else:
+            lps[i-1] = j
+            j = lps[j-1]
+    if j > 0:
+        lps[i-1] = j
+    
+    return lps
+
+
+def kmp(n: int, p: list, t: list, lps: list) -> bool:
+    j = i = 0
+    while i < 2*n - 1:
+        if p[j] == t[i]:
+            j += 1; i += 1
+            if j == n:
+                return True
+        elif not j:
+            i += 1
+        else:
+            j = lps[j-1]
+
+    return False
 
 
 print(sol(int(input())))
