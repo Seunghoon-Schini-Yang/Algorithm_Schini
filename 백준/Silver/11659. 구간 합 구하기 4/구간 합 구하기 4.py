6b89f4b55a16931c2f575d9bb8d752ca23b1b6ef
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-22 16:32:56 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 224 ms, Memory: 46264 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,33 +1,16 @@
-# binary indexed tree (fenwick tree)
+# accumulate (itertools)
 import sys
 input = sys.stdin.readline
+from itertools import accumulate
 
 
 def sol(n: int, m: int) -> str:
-    bitree = make_tree(n, list(map(int, input().split())))
-    return '\n'.join(map(str, [partial_sum(bitree, *map(int, input().split())) for _ in range(m)]))
+    acc = [0] + list(accumulate(map(int, input().split())))
+    return '\n'.join(map(str, [partial_sum(acc, *map(int, input().split())) for _ in range(m)]))
 
 
-def make_tree(n: int, arr: list) -> list:
-    bitree = [0] * (n+1)
-    for i,j in enumerate(range(1, n+1)):
-        k = j
-        while k <= n:
-            bitree[k] += arr[i]
-            k +=  k&-k
-    return bitree
-
-
-def partial_sum(tree: list, s: int, e: int) -> int:
-    return range_sum(tree, e) - range_sum(tree, s-1)
-
-
-def range_sum(tree: list, idx: int) -> int:
-    p_sum = 0
-    while idx:
-        p_sum += tree[idx]
-        idx -= idx&-idx
-    return p_sum
+def partial_sum(acc: int, s: int, e: int) -> list:
+    return acc[e] - acc[s-1]
 
 
 print(sol(*map(int, input().split())))
