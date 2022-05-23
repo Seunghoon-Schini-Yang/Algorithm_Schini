f06774c5a2203ab782b9d5f3a72dd3892df0de02
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-23 13:39:51 +0900

    [Gold III] Title: 나머지 합, Time: 484 ms, Memory: 119240 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -9,9 +9,8 @@ def sol(n: int, m: int) -> int:
     occur[0] = 1
 
     acc = 0
-    nums = map(int, input().split())
-    for _ in range(n):
-        acc = (acc+next(nums)) % m
+    for num in map(int, input().split()):
+        acc = (acc+num) % m
         occur[acc] += 1
     
     return sum(comb(i, 2) for i in occur if i > 1)
