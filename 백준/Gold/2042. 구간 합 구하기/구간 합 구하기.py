3705b596a388ddb837cbbd6fae17012177ff4a52
Seunghoon Yang <hyuio0228@gmail.com>
2022-05-23 19:49:57 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1004 ms, Memory: 116980 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,48 +1,46 @@
+# segment tree (list)
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
 
-def sol(n: int, m: int, k: int) -> str:
-    arr = [int(input()) for _ in range(n)]
-    bitree = make_tree(n, arr)
+def sol(n: int, m: int, k: int) -> None:
+    tree = [0] * (2*n + 1)
+    for i in range(n+1, 2*n+1):
+        tree[i] = int(input())
+    for i in range(2*n, 1, -1):
+        tree[i>>1] += tree[i]
 
     for _ in range(m+k):
         a,b,c = map(int, input().split())
-        if a == 1:
-            arr[b-1], c = c, c-arr[b-1]
-            update_tree(n, bitree, b, c)
+        if a&1:
+            update_tree(tree, b+n, c)
         else:
-            print(f'{partial_sum(bitree, b, c)}\n')
+            print(f'{partial_sum(tree, b+n, c+n)}\n')
     return
 
 
-def make_tree(n: int, arr: list) -> list:
-    bitree = [0] * (n+1)
-    for i,j in enumerate(range(1, n+1)):
-        k = j
-        while k <= n:
-            bitree[k] += arr[i]
-            k += k&-k
-    return bitree
-
-
-def update_tree(n: int, tree: list, idx: int, val: int) -> None:
-    while idx <= n:
+def update_tree(tree: list, idx: int, val: int) -> None:
+    val -= tree[idx]
+    while idx:
         tree[idx] += val
-        idx += idx&-idx
+        idx >>= 1
     return
 
 
 def partial_sum(tree: list, s: int, e: int) -> int:
-    return range_sum(tree, e) - range_sum(tree, s-1)
-
-
-def range_sum(tree: list, idx: int) -> int:
     p_sum = 0
-    while idx:
-        p_sum += tree[idx]
-        idx -= idx&-idx
+    while s < e:
+        if s&1:
+            p_sum += tree[s]
+            s += 1
+        if not e&1:
+            p_sum += tree[e]
+            e -= 1
+        if s < e:
+            s >>= 1; e >>= 1
+    if s == e:
+        p_sum += tree[s]
     return p_sum
 
 
