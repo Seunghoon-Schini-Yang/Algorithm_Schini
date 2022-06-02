8ba9546cce1d5f28e936bf683593751f98abf81d
Seunghoon Yang <hyuio0228@gmail.com>
2022-06-02 13:38:06 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 752 ms, Memory: 48344 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
@@ -1,24 +1,17 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 
 def sol(m: int, n: int) -> int:
-    unis = [[] for _ in range(m)]
+    unis = defaultdict(int)
     for i in range(m):
         uni = list(map(int, input().split()))
         table = {size:i for i,size in enumerate(sorted(set(uni)))}
-        unis[i] = [table[uni[i]] for i in range(n)]
-    unis.sort()
+        unis[tuple(table[uni[i]] for i in range(n))] += 1
 
-    acc = 0; cnt = 1
-    for i in range(m-1):
-        if unis[i] == unis[i+1]:
-            cnt += 1
-        else:
-            acc += cnt*(cnt-1)//2
-            cnt = 1
-    acc += cnt*(cnt-1)//2
-    return acc
-
+    # skylake 님 코드 참고 (defaultdict)
+    return sum(val*(val-1)//2 for val in unis.values())
+       
 
 print(sol(*map(int, input().split())))
