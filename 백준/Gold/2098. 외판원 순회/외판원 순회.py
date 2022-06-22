c4e482641466f1225fec8c4ccf2cf8ab2e5c75ec
Seunghoon Yang <hyuio0228@gmail.com>
2022-06-22 13:16:03 -0700

    [Gold I] Title: 외판원 순회, Time: 1516 ms, Memory: 45744 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,29 +1,30 @@
+# bitmask dp + dfs (recursive)
 import sys
 input = sys.stdin.readline
 
 
-def sol(n: int) -> int:
-    def dfs(p_n: int, mask: int) -> int:
-        if mask+2 == (1<<n):
-            if w[p_n][0]:
-                return w[p_n][0]
-            else:
-                return INF
-        
-        if memo[p_n][mask] < INF:
-            return memo[p_n][mask]
-
-        for i in range(1, n):
-            if not mask & (1<<i) and w[p_n][i]:
-                memo[p_n][mask] = min(memo[p_n][mask], dfs(i, mask|(1<<i)) + w[p_n][i])
-        return memo[p_n][mask]
-
+def dfs(node: int, mask: int) -> int:
+    if memo[node][mask] < INF:
+        return memo[node][mask]
     
-    INF = sys.maxsize
-    w = [list(map(int, input().split())) for _ in range(n)]
-    memo = [[INF] * (1 << n) for _ in range(n)]
+    if mask == (1<<n)-1:
+        memo[0][mask] = sub_INF if not weight[0][node] else weight[0][node]
+        return memo[0][mask]
+    
+    for i in range(1, n):
+        if not mask&(1<<i):
+            if not weight[i][node]:
+                weight[i][node] = sub_INF
+            memo[node][mask] = min(memo[node][mask], dfs(i, mask|(1<<i)) + weight[i][node])
+    return memo[node][mask]
 
-    return dfs(0, 0)
+
+if __name__ == '__main__':
+    n = int(input())
+    INF = pow(10, 9)
+    sub_INF = pow(10, 8)
+    weight = [list(map(int, input().split())) for _ in range(n)]
 
 
-print(sol(int(input())))
+    memo = [[INF]*(1<<n) for _ in range(n)]
+    print(dfs(0, 1))
