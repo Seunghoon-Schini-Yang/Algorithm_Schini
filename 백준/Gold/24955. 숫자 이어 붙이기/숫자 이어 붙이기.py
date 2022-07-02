49ea7ba01659fc373b2350c68aaeeb37c5043b74
Seunghoon Yang <hyuio0228@gmail.com>
2022-07-02 15:01:53 -0700

    [Gold IV] Title: 숫자 이어 붙이기, Time: 488 ms, Memory: 31308 KB -BaekjoonHub

--- 백준/Gold/24955. 숫자 이어 붙이기/숫자 이어 붙이기.py
+++ 백준/Gold/24955. 숫자 이어 붙이기/숫자 이어 붙이기.py
@@ -7,19 +7,19 @@ def dfs(p_node: int, node: int) -> str:
     global e
 
     if node == e:
-        return str(gate_nums[node])
+        return gate_nums[node]
 
     for c_node in roads[node]:
         if c_node != p_node:
             c_res = dfs(node, c_node)
             if c_res:
-                return str(gate_nums[node]) + c_res
+                return gate_nums[node] + c_res
     return False
 
 
 if __name__ == '__main__':
     N,Q = map(int, input().split())
-    gate_nums = [0] + list(map(int, input().split()))
+    gate_nums = [""] + input().split()
     roads = [[] for _ in range(N+1)]
 
     for _ in range(N-1):
