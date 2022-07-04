917b4ebeaee55197637b54ede2c3f894a5e2a0f7
Seunghoon Yang <hyuio0228@gmail.com>
2022-07-03 19:13:11 -0700

    [Gold V] Title: SHOW ME THE DUNGEON, Time: 88 ms, Memory: 39032 KB -BaekjoonHub

--- 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.py
+++ 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.py
@@ -3,10 +3,14 @@ input = sys.stdin.readline
 
 
 def dfs(mask: int, a_sum: int, ac_sum: int, p_sum: int) -> None:
+    global maxy
+
     if a_sum >= memo[mask]:
         return
+    if p_sum > maxy:
+        maxy = p_sum
     memo[mask] = a_sum
-    p_memo[mask] = p_sum
+
     
     for i in range(N):
         if not mask&(1<<i):
@@ -19,7 +23,7 @@ if __name__ == '__main__':
     attack = list(map(int, input().split()))
     popul = list(map(int, input().split()))
     memo = [K+1]*(1<<N)
-    p_memo = [0]*(1<<N)
 
+    maxy = 0
     dfs(0, 0, 0, 0)
-    print(max(p_memo))
+    print(maxy)
