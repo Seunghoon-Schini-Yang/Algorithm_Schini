dac7b450fd11113bf63a179deea6d03f4c83a0d7
Seunghoon Yang <hyuio0228@gmail.com>
2022-07-06 18:00:39 +0900

    [Platinum IV] Title: 데이터 구조, Time: 1936 ms, Memory: 160424 KB -BaekjoonHub

--- 백준/Platinum/12899. 데이터 구조/데이터 구조.py
+++ 백준/Platinum/12899. 데이터 구조/데이터 구조.py
@@ -1,57 +1,33 @@
-# segment tree
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
-N = 2_000_000
 
 
-def sol(n: int) -> None:
-    tree = [0] * (N<<1)
+def add_tree(x: int) -> None:
+    while x:
+        seg_tree[x] += 1
+        x >>= 1
 
-    for _ in range(n):
-        t,x = map(int, input().split())
-        if t == 1:
-            insert(tree, x-1+N)
+
+def del_tree(x: int) -> int:
+    i = 1
+    seg_tree[i] -= 1
+    while (i<<1) < N:
+        if seg_tree[i<<1] < x:
+            x -= seg_tree[i<<1]
+            i = (i<<1)^1
         else:
-            start = N; mid = 3*N>>1; end = N<<1
-            while start < mid:
-                p_sum = get_sum(tree, start, mid)
-                if p_sum < x:
-                    x -= p_sum
-                    start = mid
-                else:
-                    end = mid
-                mid = (start+end)>>1
-            print(f'{start-N+1}\n')
-            delete(tree, start)
-    return
+            i <<= 1
+        seg_tree[i] -= 1
+    return i-A
 
 
-def insert(tree: list, x: int) -> None:
-    while x:
-        tree[x] += 1
-        x >>= 1
-    return
-
-
-def delete(tree: list, x: int) -> None:
-    while x:
-        tree[x] -= 1
-        x >>= 1
-    return
-
-
-def get_sum(tree: list, start: int, end: int) -> int:
-    p_sum = 0
-    while start < end:
-        if start&1:
-            p_sum += tree[start]
-            start += 1
-        if end&1:
-            end ^= 1
-            p_sum += tree[end]
-        start >>= 1; end >>= 1
-    return p_sum
-
-
-sol(int(input()))
+if __name__ == '__main__':
+    N = pow(2,22); A = (N>>1)-1
+    seg_tree = [0]*N
+    for _ in range(int(input())):
+        T,X = map(int, input().split())
+        if T == 1:
+            add_tree(X+A)
+        else:
+            print(f'{del_tree(X)}\n')
