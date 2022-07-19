b6f0d0351be24bb9effd9ef30b36ec8336510cd2
Seunghoon Yang <hyuio0228@gmail.com>
2022-07-19 12:40:51 +0900

    [Platinum V] Title: LCA 2, Time: 1308 ms, Memory: 183632 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.py
+++ 백준/Platinum/11438. LCA 2/LCA 2.py
@@ -5,41 +5,56 @@ from math import log2, ceil
 
 
 def get_odr(p_node: int, node: int, depth: int) -> None:
+    global pre_odr, post_odr
+
+    pre_odr += 1
+    pre_o[node] = pre_odr
     depths[node] = depth
     parents[node] = p_node
+
     for c_n in tree[node]:
         if c_n != p_node:
             get_odr(node, c_n, depth+1)
+    
+    post_odr += 1
+    post_o[node] = post_odr
     return
+
+
+def is_ancestor(n1: int, n2: int) -> bool:
+    if pre_o[n1] < pre_o[n2] and post_o[n1] > post_o[n2]:
+        return True
+    return False
 
 
 def get_lca(n1: int, n2: int) -> int:
     if n1 == n2:
         return n1
-    if depths[n1] < depths[n2]:
+    if depths[n1] > depths[n2]:
         n1,n2 = n2,n1
+    if is_ancestor(n1, n2):
+        return n1 if depths[n1] < depths[n2] else n2
 
-    gap = depths[n1]-depths[n2]
-    for i in range(logN):
-        if gap&(1<<i):
-            n1 = sparse[i][n1]
-
-    if n1 == n2:
-        return n1
-    
-    for i in range(logN-1, -1, -1):
-        if sparse[i][n1] != sparse[i][n2]:
-            n1 = sparse[i][n1]
-            n2 = sparse[i][n2]
-    return sparse[0][n1]
+    lo = n1; hi = sparse[logN-1][n1]
+    for i in range(logN-2, -1, -1):
+        mid = sparse[i][lo]
+        if is_ancestor(mid, n2):
+            hi = mid
+        else:
+            lo = mid
+    return hi
 
 
 if __name__ == '__main__':
+
     # init
     N = int(input())
     tree = [[] for _ in range(N+1)]
+    pre_o = [0]*(N+1)
+    post_o = [0]*(N+1)
     parents = [0]*(N+1)
     depths = [0]*(N+1)
+    pre_odr = post_odr = 0
 
     # get_tree
     for _ in range(N-1):
@@ -47,11 +62,11 @@ if __name__ == '__main__':
         tree[a].append(b)
         tree[b].append(a)
 
-    # get parents & depth
-    get_odr(0, 1, 0)
+    # get pre, post odr & depth
+    get_odr(0, 1, 1)
 
     # get sparse table
-    logN = ceil(log2(max(depths)))+1
+    logN = ceil(log2(max(depths)-1))+1
     sparse = [[0]*(N+1) for _ in range(logN)]
     sparse[0] = parents
     sparse[0][1] = 1
