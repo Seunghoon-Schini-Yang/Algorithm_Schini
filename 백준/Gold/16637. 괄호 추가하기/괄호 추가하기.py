31d79384d6a234673e45c61a9bebf1538f317988
Seunghoon Yang <hyuio0228@gmail.com>
2022-08-14 14:19:11 +0900

    [Gold IV] Title: 괄호 추가하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -11,27 +11,49 @@ def calc(a: int, symbol: str, b: int) -> int:
         return a*b
 
 
-def dfs(cur_idx: int, cur_res: int) -> None:
-    global maxy
-    
+def minmax(a: int, b: int) -> tuple:
+    return (a,b) if b > a else (b,a)
+
+
+def dfs(cur_idx: int) -> None:
     if cur_idx == sym_len:
-        maxy = max(cur_res, maxy)
         return
-    dfs(cur_idx+1, calc(cur_res, symbols[cur_idx], nums[cur_idx+1]))
+    n_min, n_max = minmax(calc(memo_max[cur_idx], symbols[cur_idx], nums[cur_idx+1]), calc(memo_min[cur_idx], symbols[cur_idx], nums[cur_idx+1]))
+    is_dfs = False
+    if memo_max[cur_idx+1] < n_max:
+        memo_max[cur_idx+1] = n_max
+        is_dfs = True
+    if memo_min[cur_idx+1] > n_min:
+        memo_min[cur_idx+1] = n_min
+        is_dfs = True
+    if is_dfs:
+        dfs(cur_idx+1)
     
     if cur_idx+1 == sym_len:
         return
-    dfs(cur_idx+2, calc(cur_res, symbols[cur_idx], adj_cal[cur_idx+1]))
+    n_min, n_max = minmax(calc(memo_max[cur_idx], symbols[cur_idx], adj_cal[cur_idx+1]), calc(memo_min[cur_idx], symbols[cur_idx], adj_cal[cur_idx+1]))
+    is_dfs = False
+    if memo_max[cur_idx+2] < n_max:
+        memo_max[cur_idx+2] = n_max
+        is_dfs = True
+    if memo_min[cur_idx+2] > n_min:
+        memo_min[cur_idx+2] = n_min
+        is_dfs = True
+    if is_dfs:
+        dfs(cur_idx+2)
     return
 
 
 if __name__ == '__main__':
-    N = int(input()); sym_len = N >> 1
+    INF = sys.maxsize
+    sym_len = int(input()) >> 1
     equation = input().rstrip()
     nums = list(map(int, equation[::2]))
     symbols = equation[1::2]
     adj_cal = [calc(nums[i], symbols[i], nums[i+1]) for i in range(sym_len)]
-    maxy = -sys.maxsize
+    memo_max = [-INF]*(sym_len+1)
+    memo_min = [INF]*(sym_len+1)
+    memo_max[0] = memo_min[0] = nums[0]
     
-    dfs(0, nums[0])
-    print(maxy)
+    dfs(0)
+    print(memo_max[-1])
