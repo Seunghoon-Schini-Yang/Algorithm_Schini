933f1772f2a9a098c7f290607528ac3a50458c5f
Seunghoon Yang <hyuio0228@gmail.com>
2022-08-20 13:25:20 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+
+
+def solution(key, lock):
+    def rotate():
+        rotated = [[0]*n for _ in range(m)]
+        for r in range(m):
+            for c in range(m):
+                rotated[r][c] = key[~c][r]
+        return rotated
+    
+    
+    def is_compatible(r, c):
+        for rr in range(m):
+            for cc in range(m):
+                new_lock[r+rr][c+cc] += key[rr][cc]
+        
+        if [new_lock[r][m-1:m-1+n] for r in range(m-1, m-1+n)] == dstn:
+            return True
+    
+        for rr in range(m):
+            for cc in range(m):
+                new_lock[r+rr][c+cc] -= key[rr][cc]
+            
+        return False
+    
+    
+    n, m = len(lock), len(key)
+    new_lock = [[0]*(n+2*m-2) for _ in range(m-1)]
+    for i in range(n):
+        new_lock.append([0]*(m-1) + lock[i] + [0]*(m-1))
+    new_lock.extend([[0]*(n+2*m-2) for _ in range(m-1)])
+    
+    dstn = [[1]*n for _ in range(n)]
+    for _ in range(4):
+        key = rotate()
+        for r in range(n+m-1):
+            for c in range(n+m-1):
+                if is_compatible(r, c):
+                    return True
+    
+    return False
