b53d01c2e1281a38eb324ec7bc84723fa5b2a06c
Seunghoon Yang <hyuio0228@gmail.com>
2022-09-28 00:56:08 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    if n&1:
+        return 2
+    
+    n -= 1
+    for i in range(3, int(pow(n, 0.5))+1, 2):
+        if not n % i:
+            return i
+    return n
+    
+    
+    
+    
+    
+    return answer
