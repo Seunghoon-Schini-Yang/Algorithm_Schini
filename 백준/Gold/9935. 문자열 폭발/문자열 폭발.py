aee306b59c1110afe9262de95f455a3462e4c30e
Seunghoon Yang <hyuio0228@gmail.com>
2022-11-08 17:59:21 +0900

    [Gold IV] Title: 문자열 폭발, Time: 616 ms, Memory: 37364 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == '__main__':
+    string = input().strip(); s_len = len(string); si= 0
+    key = input().strip(); k_len = len(key); ki = 0
+    stack = list()
+    result = ''
+
+    while si < s_len:
+        if ki == k_len:
+            ki = stack.pop() if stack else 0
+        elif string[si] == key[ki]:
+            si += 1; ki += 1
+        # string[si] != key[ki]
+        elif not ki:
+            for i in stack:
+                result += key[:i]
+            stack = list()
+            result += string[si]
+            si += 1
+        else:
+            stack.append(ki)
+            ki = 0
+
+    for i in stack:
+        result += key[:i]
+    if ki != k_len:
+        result += key[:ki]
+
+    print(result if result else 'FRULA')
