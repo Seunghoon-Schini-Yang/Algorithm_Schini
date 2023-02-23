3b502a7504ad3b7a246d1809f4dad058f5afd669
Seunghoon Yang <hyuio0228@gmail.com>
2023-02-23 13:35:52 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 65.76 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,33 +1,39 @@
-def solution(queue1, queue2):
-    answer = 0
-    circle_arr = queue1 + queue2
-    optimal = sum(circle_arr)
-    if optimal % 2:
+def solution(q1: list, q2: list) -> int:
+    def get_count(i: int, j: int) -> int:
+        if i >= h_n:
+            i -= h_n; j -= h_n
+        return i+j-h_n if j >= h_n else i+j+h_n
+        
+        
+    q = q1 + q2
+    n = len(q); h_n = n >> 1
+    elem_sum = sum(q)
+
+    if elem_sum & 1:
         return -1
-    optimal = optimal // 2
-    a, b = 0, len(queue1)
-    A = sum(circle_arr[a:b])
+    elem_sum >>= 1
     
-    # 두 큐의 합이 같을 때 까지 함
-    while A != optimal:
-        fail = True
-        # a를 시계방향으로 이동
-        while A > optimal and a <= b:
-            A -= circle_arr[a]
-            a += 1
-            answer += 1
-            fail = False
-        # b를 시계방향으로 이동
-        while A < optimal and b < len(circle_arr):
-            A += circle_arr[b]
-            b += 1      
-            answer += 1
-            fail = False   
-        # 답이 없음 
-        if len(circle_arr) <= a or fail:
-            return -1
-           
-    return answer
-
-
-print(solution([1, 3, 3, 1], [1, 1, 1, 1]))
+    # two pointer
+    i = 0; j = 1
+    cur_sum = q[0]
+    cnt = 900000
+    while True:
+        if cur_sum < elem_sum:
+            if j == n:
+                break
+            cur_sum += q[j]
+            j += 1
+        elif cur_sum > elem_sum:
+            if i == j:
+                break          
+            cur_sum -= q[i]
+            i += 1
+        else:
+            # count
+            cnt = min(cnt, get_count(i, j))
+            if j == n:
+                break
+            cur_sum -= q[i]; cur_sum += q[j]
+            i += 1; j += 1
+    
+    return -1 if cnt == 900000 else cnt
