09839fcd1c0b78ee930cc0125011435fff80a552
Seunghoon Yang <hyuio0228@gmail.com>
2023-02-28 11:52:03 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2700 ms, Memory: 136588 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -2,63 +2,43 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
+
 def sol(n: int, m: int) -> int:
-    cnt = 0
+    def adjs(i: int, j: int) -> tuple:
+        if i > 0:  yield i-1, j
+        if j > 0:  yield i, j-1
+        if i < n-1:  yield i+1, j
+        if j < m-1:  yield i, j+1
+
+    
+    cnt = 1
     locs = [input().rstrip() for _ in range(n)]
-    visited = [[[0,0] for _ in range(m)] for _ in range(n)]
+    is_visited = [[[False, False] for _ in range(m)] for _ in range(n)]
+    is_visited[0][0] = [True, True]
     q = deque([(0,0,0)])
 
     while q:
         for _ in range(len(q)):
-            i,j,flag = q.popleft()
+            r,c,flag = q.popleft()
 
-            if i==n-1 and j==m-1:
-                return cnt+1
+            if r==n-1 and c==m-1:
+                return cnt
 
-            if flag:
-                if i>0 and not visited[i-1][j][1] and locs[i-1][j]=='0':
-                    q.append((i-1,j,1))
-                    visited[i-1][j][1] = 1
-                if i<n-1 and not visited[i+1][j][1] and locs[i+1][j]=='0':
-                    q.append((i+1,j,1))
-                    visited[i+1][j][1] = 1
-                if j>0 and not visited[i][j-1][1] and locs[i][j-1]=='0':
-                    q.append((i,j-1,1))
-                    visited[i][j-1][1] = 1
-                if j<m-1 and not visited[i][j+1][1] and locs[i][j+1]=='0':
-                    q.append((i,j+1,1))
-                    visited[i][j+1][1] = 1
-            else:
-                if i>0:
-                    if not visited[i-1][j][0] and locs[i-1][j] =='0':
-                        q.append((i-1,j,0))
-                        visited[i-1][j][0] = 1
-                    elif not visited[i-1][j][1] and locs[i-1][j] =='1':
-                        q.append((i-1,j,1))
-                        visited[i-1][j][1] = 1
-                if i<n-1:
-                    if not visited[i+1][j][0] and locs[i+1][j] =='0':
-                        q.append((i+1,j,0))
-                        visited[i+1][j][0] = 1
-                    elif not visited[i+1][j][1] and locs[i+1][j] =='1':
-                        q.append((i+1,j,1))
-                        visited[i+1][j][1] = 1
-                if j>0:
-                    if not visited[i][j-1][0] and locs[i][j-1] =='0':
-                        q.append((i,j-1,0))
-                        visited[i][j-1][0] = 1
-                    elif not visited[i][j-1][1] and locs[i][j-1] =='1':
-                        q.append((i,j-1,1))
-                        visited[i][j-1][1] = 1
-                if j<m-1:
-                    if not visited[i][j+1][0] and locs[i][j+1] =='0':
-                        q.append((i,j+1,0))
-                        visited[i][j+1][0] = 1
-                    elif not visited[i][j+1][1] and locs[i][j+1] =='1':
-                        q.append((i,j+1,1))
-                        visited[i][j+1][1] = 1
+            for i, j in adjs(r, c):
+                if flag:
+                    if is_visited[i][j][1]:
+                        continue
+                    if locs[i][j] == '0':
+                        is_visited[i][j][1] = True
+                        q.append((i,j,1))
+                else:
+                    if is_visited[i][j][0]:
+                        continue
+                    is_visited[i][j][0] = True
+                    q.append((i,j,0)) if locs[i][j] == '0' else q.append((i,j,1))
         cnt += 1
     return -1
 
 
 print(sol(*map(int, input().split())))
+
