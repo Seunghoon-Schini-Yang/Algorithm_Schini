060f2275252ab543d3527c10074f58d95dd214ad
Seunghoon Yang <hyuio0228@gmail.com>
2023-03-01 12:42:07 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,63 +1,39 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-# Check if there is a winning row, column, or diagonal
-def check_win(player, board):
-    # Check rows
-    for i in range(3):
-        if all(cell == player for cell in board[i]):
-            return True
-
-    # Check columns
-    for j in range(3):
-        if all(board[i][j] == player for i in range(3)):
-            return True
-
-    # Check diagonals
-    if all(board[i][i] == player for i in range(3)):
-        return True
-    if all(board[i][2-i] == player for i in range(3)):
-        return True
-
-    return False
-
 def solution(board):
-    num_x = sum(row.count('X') for row in board)
-    num_o = sum(row.count('O') for row in board)
-
-    if num_x - num_o > 0 or abs(num_x - num_o) > 1:
+    o = x = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 'O':
+                o += 1
+            elif board[i][j] == 'X':
+                x += 1
+    
+    if not (o == x or o == x+1):
         return 0
 
-    elif (check_win('O', board) and num_x != num_o - 1) or (check_win('X', board) and num_x != num_o):
+    olink = set(); xlink = set()
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            if board[i][0] == 'O':
+                olink.add((i, 0)); olink.add((i, 1)); olink.add((i, 2))
+            elif board[i][0] == 'X':
+                xlink.add((i, 0)); xlink.add((i, 1)); xlink.add((i, 2))
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i]:
+            if board[0][i] == 'O':
+                olink.add((0, i)); olink.add((1, i)); olink.add((2, i))
+            elif board[0][i] == 'X':
+                xlink.add((0, i)); xlink.add((1, i)); xlink.add((2, i))
+    if board[0][0] == board[1][1] == board[2][2]:
+        if board[0][0] == 'O':
+            olink.add((0, 0)); olink.add((1, 1)); olink.add((2, 2))
+        elif board[0][0] == 'X':
+            xlink.add((0, 0)); xlink.add((1, 1)); xlink.add((2, 2))
+    if board[0][2] == board[1][1] == board[2][0]:
+        if board[0][2] == 'O':
+            olink.add((0, 2)); olink.add((1, 1)); olink.add((2, 0))
+        elif board[0][2] == 'X':
+            xlink.add((0, 2)); xlink.add((1, 1)); xlink.add((2, 0))
+    
+    if (6 <= len(olink) + len(xlink)) or (olink and o == x) or (xlink and x < o):
         return 0
-
     return 1
