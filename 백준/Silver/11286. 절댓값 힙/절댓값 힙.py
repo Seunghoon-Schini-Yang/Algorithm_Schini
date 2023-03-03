1f2cf3e1d09f49835bcc8567cd848426f7d3f7c6
Seunghoon Yang <hyuio0228@gmail.com>
2023-03-04 00:51:03 +0900

    [Silver I] Title: 절댓값 힙, Time: 112 ms, Memory: 39532 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,25 +1,28 @@
 import heapq
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
+
 def solution(n: int) -> None:
-    prior_q = list()
-    dd = defaultdict(int)
+    answer = []
+    prior_q = []
+    dd = {}
     for _ in range(n):
         if (num := int(input())):
             if num < 0:
-                dd[(num := abs(num))] += 1
+                dd.update({(num := -num): dd.get(num, 0)+1})
             heapq.heappush(prior_q, num)
         else:
             if prior_q:
-                if dd[(num := heapq.heappop(prior_q))]:
-                    print(-num)
+                if dd.get((num := heapq.heappop(prior_q)), 0):
+                    answer.append(-num)
                     dd[num] -= 1
                 else:
-                    print(num)
+                    answer.append(num)
             else:
-                print(0)
+                answer.append(0)
+    
+    return '\n'.join(map(str, answer))
 
 
-solution(int(input()))
+print(solution(int(input())))
