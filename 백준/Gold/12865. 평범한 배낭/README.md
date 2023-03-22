68bc73f8518d67d5f5b44ebcba1c3fb1210f6002
Seunghoon Yang <hyuio0228@gmail.com>
2023-03-22 12:19:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 516 ms, Memory: 37756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/README.md
+++ 백준/Gold/12865. 평범한 배낭/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 33688 KB, 시간: 1700 ms
+메모리: 37756 KB, 시간: 516 ms
 
 ### 분류
 
-다이나믹 프로그래밍(dp), 배낭 문제(knapsack)
+Dynamic Programming, Knapsack
 
 ### 문제 설명
 
--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,17 +2,19 @@ import sys
 input = sys.stdin.readline
 
 
-def solution(info: map) -> int:
-    n, w_limit = info
-    # pairs = [tuple(map(int, input().split())) for _ in range(n)]
-    dp = [0] * (w_limit + 1)
+def main():
+    N, K = map(int, input().split())
+    memo = {0: 0}
+    for _ in range(N):
+        W, V = map(int, input().split())
+        tmp = {}
+        for v, w in memo.items():
+            if w+W > memo.get(v+V, K):
+                continue
+            tmp[v+V] = w+W
+        memo.update(tmp)
+    print(max(memo.keys()))
 
-    for w, v in [map(int, input().split()) for _ in range(n)]:
-        for i in range(w_limit, w - 1, -1):
-            if dp[i - w] + v > dp[i]:
-                dp[i] = dp[i - w] + v
-    
-    return dp[w_limit]
 
-
-print(solution(map(int, input().split())))
+if __name__ == '__main__':
+    main()
