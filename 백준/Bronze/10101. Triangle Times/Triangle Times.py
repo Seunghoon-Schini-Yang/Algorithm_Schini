559255066e30bd72150ba64f3c1f5044df5dc7b6
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-15 00:30:26 +0900

    [Bronze IV] Title: Triangle Times, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. Triangle Times/Triangle Times.py
@@ -0,0 +1,19 @@
+def main():
+    _sum = 0
+    angles = set()
+    for i in range(3):
+        _sum += (angle := int(input()))
+        angles.add(angle)
+    if _sum != 180:
+        return 'Error'
+    n = len(angles)
+    if n == 1:
+        return 'Equilateral'
+    if n == 2:
+        return 'Isosceles'
+    return 'Scalene'
+
+
+if __name__ == '__main__':
+    _type = main()
+    print(_type)
