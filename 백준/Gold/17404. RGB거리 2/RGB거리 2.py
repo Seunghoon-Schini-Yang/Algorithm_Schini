f538128da4bb3e46834bc463030a557b6eba35f5
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-19 14:23:06 +0900

    [Gold IV] Title: RGB거리 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -2,21 +2,22 @@ import sys
 input = sys.stdin.readline
 
 
-def sol(n: int) -> int:
-    INF = sys.maxsize
+def main(N):
+    INF = (N+1) * 1000
+    R, G, B  = map(int, input().split())
+    R2, G2, B2  = map(int, input().split())
+    rr = gg = bb = INF
+    rb, rg, gr, gb, br, bg =\
+        R+B2, R+G2, G+R2, G+B2, B+R2, B+G2
+    for _ in range(N-2):
+        R, G, B = map(int, input().split())
+        rr, rg, rb, gr, gg, gb, br, bg, bb =\
+        min(rg, rb) + R, min(rr, rb) + G, min(rr, rg) + B,\
+        min(gg, gb) + R, min(gr, gb) + G, min(gr, gg) + B,\
+        min(bg, bb) + R, min(br, bb) + G, min(br, bg) + B
+    return min(rg, rb, gr, gb, br, bg)
 
-    rgb = map(int, input().split())
-    memo = [[INF]*3 for i in range(3)]
-    for i in range(3):
-        memo[i][i] = next(rgb)
-    
-    for i in range(n-1):
-        rgb = tuple(map(int, input().split()))
-        for f_c in range(3):
-            to_paint = (min(memo[f_c][1], memo[f_c][2]), min(memo[f_c][2], memo[f_c][0]), min(memo[f_c][0], memo[f_c][1]))
-            memo[f_c][:] = [cur+pre for cur, pre in zip(rgb, to_paint)]
 
-    return min(memo[i][j] for j in range(3) for i in range(3) if i != j)
-
-
-print(sol(int(input())))
+if __name__ == '__main__':
+    lowest_cost = main(int(input()))
+    print(lowest_cost)
