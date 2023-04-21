2155e440d3c3a9cd1506b54f4491bb9be6ab3fd2
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-21 17:16:30 +0900

    [Gold V] Title: 동전 1, Time: 116 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+
 def solution(n: int, k: int) -> int:
     dp = [0] * (k+1)
     dp[0] = 1
-
     for _ in range(n):
         coin = int(input())
         for i in range(coin, k+1):
-            dp[i] = dp[i-coin] + dp[i]
-
+            if not dp[i-coin]:
+                continue
+            dp[i] += dp[i-coin]
     return dp[k]
 
 
