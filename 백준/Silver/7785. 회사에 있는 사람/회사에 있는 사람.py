fdae902275beabd00f14f7d1131b44ace04c70d2
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-23 11:19:54 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4004 ms, Memory: 42864 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,9 @@
+n = int(input())
+current = set()
+for _ in range(n):
+    _name, is_enter = input().split()
+    if is_enter[0] == 'e':
+        current.add(_name)
+    else:
+        current.discard(_name)
+print('\n'.join(sorted(current, reverse=True)))
