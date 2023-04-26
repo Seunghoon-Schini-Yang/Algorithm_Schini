519c15e23c1926dec832faf79bfc8884d2b24e3e
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-26 14:04:15 +0900

    [level 3] Title: 양과 늑대, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,30 +1,28 @@
-answer = 0
-
-
 def solution(info: list, edges: list) -> int:
     def dfs(pn: int, sheep: int, wolf: int, an_list: set):
-        global answer
+        nonlocal answer
 
         if not info[pn]:
             sheep += 1
             answer = max(answer, sheep)
         else:
             wolf += 1
-        
+
         if sheep <= wolf:
             return
-        
+
         for cn in an_list:
             nan_list = an_list.copy()
             nan_list.remove(cn)
-            nan_list |= graph[cn]
+            for ccn in graph[cn]:
+                nan_list.add(ccn)
             dfs(cn, sheep, wolf, nan_list)
         return
 
-
+    
+    answer = 0
     graph = [set() for _ in range(len(info))]
     for p, c in edges:
         graph[p].add(c)
-    
     dfs(0, 0, 0, graph[0])
     return answer
