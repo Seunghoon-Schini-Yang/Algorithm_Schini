4e89274b072ccca070580103d7b4ac1eda149c7d
Seunghoon Yang <hyuio0228@gmail.com>
2023-04-30 02:50:33 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 920 ms, Memory: 41444 KB -BaekjoonHub

---
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+
+def scatter(d, r, c):
+    dr, dc = drr[d], dcc[d]
+    update(r+(2*dr), c+(2*dc), board[r][c]//20)
+    update(r+dr+dc, c+dc+dr, board[r][c]//10)
+    update(r+dr-dc, c+dc-dr, board[r][c]//10)
+    update(r+(2*dc), c+(2*dr), board[r][c]//50)
+    update(r-(2*dc), c-(2*dr), board[r][c]//50)
+    update(r+dc, c+dr, board[r][c]*7//100)
+    update(r-dc, c-dr, board[r][c]*7//100)
+    update(r-dr+dc, c-dc+dr, board[r][c]//100)
+    update(r-dr-dc, c-dc-dr, board[r][c]//100)
+    alpha = board[r][c] - board[r][c]//20 - (board[r][c]//10 + board[r][c]//50 + board[r][c]//100 + board[r][c]*7//100)*2
+    update(r+dr, c+dc, alpha)
+    board[r][c] = 0
+    return
+
+
+def update(r, c, amount):
+    global out
+    if 0 <= r < N and 0 <= c < N:
+        board[r][c] += amount
+    else:
+        out += amount
+    
+        
+if __name__ == '__main__':
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    out = d = 0
+    r = c = N>>1
+    drr = [0, 1, 0, -1]
+    dcc = [-1, 0, 1, 0]
+    for i in range(1, N):
+        for _ in range(2):
+            for _ in range(i):
+                r += drr[d]
+                c += dcc[d]
+                scatter(d, r, c)
+            d = (d+1)%4
+    for _ in range(N-1):
+        r += drr[d]
+        c += dcc[d]
+        scatter(d, r, c)
+    print(out)
