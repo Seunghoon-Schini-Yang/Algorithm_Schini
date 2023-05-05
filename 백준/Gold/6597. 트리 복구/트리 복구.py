036f4673210b32de96dfeb3718c4385403f82891
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-05 12:39:05 +0900

    [Gold III] Title: 트리 복구, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/6597. 트리 복구/트리 복구.py
+++ 백준/Gold/6597. 트리 복구/트리 복구.py
@@ -1,15 +1,13 @@
 import sys
 
 
-def recur(preo, ino):
+def get_posto(preo, ino):
     if len(preo) <= 1:
         return preo
-    _key = preo[0]
-    k_i = ino.index(_key)
-    return recur(preo[1:k_i+1], ino[:k_i]) + recur(preo[k_i+1:], ino[k_i+1:]) + _key
+    k_i = ino.index(preo[0])
+    return get_posto(preo[1:k_i+1], ino[:k_i]) + get_posto(preo[k_i+1:], ino[k_i+1:]) + preo[0]
 
 
 if __name__ == '__main__':
     for query in sys.stdin.readlines():
-        print(recur(*query.split()))
-        
+        print(get_posto(*query.split()))
