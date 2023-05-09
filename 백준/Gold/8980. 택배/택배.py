0ffa48e724fbaa66c7793af9b7719dc66132ed22
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-10 01:21:46 +0900

    [Gold II] Title: 택배, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -3,23 +3,23 @@ input = sys.stdin.readline
 
 
 def _get(l, r):
-    l += N-1; r += N
+    l += N-1; r += N-1
     _max = 0
     while l < r:
         if l&1:
-            _max = max(_max, tree[l])
+            _max += tree[l]
             l += 1
         if r&1:
             r ^= 1
-            _max = max(_max, tree[r])
+            _max += tree[r]
         l >>= 1; r >>= 1
     return _max
 
 
 def _update(node, val):
     node += N-1
-    while node and tree[node] < val:
-        tree[node] = val
+    while node:
+        tree[node] += val
         node >>= 1
     return
 
@@ -28,13 +28,13 @@ if __name__ == '__main__':
     N, C = map(int, input().split())
     M = int(input())
     ts = [tuple(map(int, input().split())) for _ in range(M)]
-    ts.sort(key=lambda x: x[1])
+    ts.sort(key=lambda x: (x[1], x[0]))
     
-    ans = 0
     tree = [0] * (N<<1)
     for s, r, v in ts:
-        _max = _get(s, r-1)
+        _max = _get(s, r)
+        if C <= _max:  continue
+        free = C - _max
         v = min(C, v)
-        ans += min(v, C-_max)
-        _update(r-1, min(C, _max+v))
-    print(ans)
+        _update(r-1, min(v, free))
+    print(_get(1, N))
