fccd0306b7c1fefb887dfac363fb8af58c784fa7
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-10 01:49:01 +0900

    [Gold II] Title: 택배, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -2,39 +2,20 @@ import sys
 input = sys.stdin.readline
 
 
-def _get(l, r):
-    l += N-1; r += N-1
-    _max = 0
-    while l < r:
-        if l&1:
-            _max += tree[l]
-            l += 1
-        if r&1:
-            r ^= 1
-            _max += tree[r]
-        l >>= 1; r >>= 1
-    return _max
-
-
-def _update(node, val):
-    node += N-1
-    while node:
-        tree[node] += val
-        node >>= 1
-    return
-
-
 if __name__ == '__main__':
     N, C = map(int, input().split())
     M = int(input())
     ts = [tuple(map(int, input().split())) for _ in range(M)]
-    ts.sort(key=lambda x: (x[1], x[0]))
+    ts.sort(key=lambda x: x[1])
     
-    tree = [0] * (N<<1)
+    tree = [0] * N
+    ans = 0
     for s, r, v in ts:
-        _max = _get(s, r)
-        if C <= _max:  continue
+        _max = max(tree[s-1:r])
+        if _max == C:  continue
         free = C - _max
-        v = min(C, v)
-        _update(r-1, min(v, free))
-    print(_get(1, N))
+        load = min(free, v)
+        ans += load
+        for i in range(s-1, r):
+            tree[i] += load
+    print(ans)
