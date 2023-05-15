072cfd7f27b6a3414bd28b86d963668853f78350
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-15 11:58:54 +0900

    [Gold II] Title: 로봇 조종하기, Time: 1236 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+
+    prev = list(map(int, input().split()))
+    for i in range(1, M):
+        prev[i] += prev[i-1]
+
+    for _ in range(N-1):
+        cur = list(map(int, input().split()))
+        l = [0] * M
+        r = [0] * M
+        l[0] = prev[0] + cur[0]
+        r[~0] = prev[~0] + cur[~0]
+        for i in range(1, M):
+            l[i] = max(prev[i], l[i-1]) + cur[i]
+            r[~i] = max(prev[~i], r[~(i-1)]) + cur[~i]
+        cur = [max(ll, rr) for ll, rr in zip(l, r)]
+        prev = cur
+    print(prev[-1])
