a0a72950d9301ced40812d4532125c86f872e4a3
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-17 15:11:37 +0900

    [Gold II] Title: 문제집, Time: 196 ms, Memory: 40516 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,32 +1,26 @@
-# topological sort + bfs + heap
 import sys
 input = sys.stdin.readline
-print = sys.stdout.write
-from heapq import heappop, heappush
+from heapq import heapify, heappush, heappop
 
 
-def sol(n: int, m: int) -> str:
-    graph = [[] for _ in range(n+1)]
-    indgr = [0]*(n+1)
-
-    for _ in range(m):
-        f,b = map(int, input().split())
-        graph[f].append(b)
-        indgr[b] += 1
-
-    hq = list()
-    for i in range(1, n+1):
-        if not indgr[i]:
-            heappush(hq, i)
-
-    while hq:
-        v = heappop(hq)
-        print(f'{v} ')
-        for node in graph[v]:
-            indgr[node] -= 1
-            if not indgr[node]:
-                heappush(hq, node)
-    return
+def _pop():
+    p = heappop(pq)
+    for c in graph[p]:
+        dgr[c] -= 1
+        if not dgr[c]:
+            heappush(pq, c)
+    return p
 
 
-sol(*map(int, input().split()))
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    dgr = [0] * (N+1)
+    for _ in range(M):
+        p, c = map(int, input().split())
+        graph[p].append(c)
+        dgr[c] += 1
+    
+    pq = [i for i in range(1, N+1) if not dgr[i]]
+    heapify(pq)
+    print(*[_pop() for _ in range(N)])
