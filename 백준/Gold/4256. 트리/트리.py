6a3a945d470ea108865b19f6567504551d4f6a0b
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-18 15:34:52 +0900

    [Gold II] Title: 트리, Time: 264 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.py
+++ 백준/Gold/4256. 트리/트리.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 
 def get_post(ps, pe, os, oe):
-    if ps > pe:  return []
-    if ps == pe:  return [pre[ps]]
+    if ps > pe:  return ''
+    if ps == pe:  return f'{pre[ps]} '
     idx = ino[pre[ps]]
-    return get_post(ps+1, ps+idx-os, os, idx-1) + get_post(ps+1+idx-os, pe, idx+1, oe) + [pre[ps]]
+    return get_post(ps+1, ps+idx-os, os, idx-1) + get_post(ps+1+idx-os, pe, idx+1, oe) + f'{pre[ps]} '
 
 
 if __name__ == '__main__':
@@ -14,4 +14,4 @@ if __name__ == '__main__':
         n = int(input())
         pre = input().split()
         ino = {v: i for i, v in enumerate(input().split())}
-        print(' '.join(get_post(0, n-1, 0, n-1)))
+        print(get_post(0, n-1, 0, n-1))
