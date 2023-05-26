c11ca87c91e2c1b3247f55d711aa1ae1e8d9c604
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-27 02:01:17 +0900

    [level 3] Title: 숫자 게임, Time: 38.09 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,9 +1,3 @@
 def solution(A, B):
-    A.sort()
-    B.sort()
-    i = score = 0
-    for b in B:
-        if A[i] < b:
-            i += 1
-            score += 1
-    return score
+    answer = -1
+    return answer
