abed0362d38be9005bb01d53e269051252b7cd7c
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-29 23:20:34 +0900

    [Gold II] Title: 숫자 박스, Time: 7744 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/1983. 숫자 박스/숫자 박스.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+
+def check(i, r, c):
+    maxy = 0
+    if 0 <= i-r < erl and 0 <= i-c < ecl:
+        maxy = memo[r][c] + row[i-r]*col[i-c]
+        if 0 < r:
+            maxy = max(maxy, memo[r-1][c])
+        if 0 < c:
+            maxy = max(maxy, memo[r][c-1])
+        memo[r][c] = maxy
+
+
+if __name__ == '__main__':
+    N = int(input())
+    row = [r for r in map(int, input().split()) if r]
+    col = [c for c in map(int, input().split()) if c]
+    rl = N-len(row)+1
+    cl = N-len(col)+1
+    erl, ecl = len(row), len(col)
+    memo = [[0]*cl for _ in range(rl)]
+    
+    for i in range(N):
+        for r in range(rl-1, -1, -1):
+            for c in range(cl-1, -1, -1):
+                check(i, r, c)
+    print(max(max(row) for row in memo))
