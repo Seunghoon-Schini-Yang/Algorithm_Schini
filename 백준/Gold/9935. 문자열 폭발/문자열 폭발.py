0466018945b6102d3afb1454ce569846e9e7fdb1
Seunghoon Yang <hyuio0228@gmail.com>
2023-05-30 13:45:36 +0900

    [Gold IV] Title: 문자열 폭발, Time: 584 ms, Memory: 53536 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -3,30 +3,31 @@ input = sys.stdin.readline
 
 
 if __name__ == '__main__':
-    string = input().strip(); s_len = len(string); si= 0
-    key = input().strip(); k_len = len(key); ki = 0
-    stack = list()
-    result = ''
-
-    while si < s_len:
-        if ki == k_len:
-            ki = stack.pop() if stack else 0
-        elif string[si] == key[ki]:
-            si += 1; ki += 1
-        # string[si] != key[ki]
-        elif not ki:
-            for i in stack:
-                result += key[:i]
-            stack = list()
-            result += string[si]
-            si += 1
+    string = input().rstrip()
+    bomb = input().rstrip()
+    lb, ls = len(bomb), len(string)
+    stack = []
+    cur = []
+    
+    si = 0
+    i = 0
+    while si < ls:
+        if i == lb:
+            del cur[-lb:]
+            i = stack.pop() if stack else 0
+            continue
+        if string[si] == bomb[i]:
+            i += 1
         else:
-            stack.append(ki)
-            ki = 0
-
-    for i in stack:
-        result += key[:i]
-    if ki != k_len:
-        result += key[:ki]
-
-    print(result if result else 'FRULA')
+            if i and string[si] == bomb[0]:
+                stack.append(i)
+                i = 1
+            else:
+                stack = []
+                i = 0
+        cur.append(string[si])
+        si += 1
+    
+    if i == lb:
+        del cur[-lb:]
+    print(''.join(map(str, cur)) if cur else 'FRULA')
