1eb835ed8f987b7c388b4d7a133ce220e216ad9d
Seunghoon Yang <hyuio0228@gmail.com>
2023-06-10 11:16:32 +0900

    [Gold V] Title: 탑, Time: 648 ms, Memory: 138736 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     stack = [(0, 100_000_001)]
     rs = [0] * N
     for i, h in enumerate(map(int, input().split())):
-        while stack and stack[-1][1] < h:
+        while stack[-1][1] < h:
             stack.pop()
         rs[i] = stack[-1][0]
         stack.append((i+1, h))
