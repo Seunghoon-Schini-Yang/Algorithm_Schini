eabd3b1823a9aeb6e1f0e033167ba759b19af604
Seunghoon Yang <hyuio0228@gmail.com>
2023-06-19 15:12:43 +0900

    [level 3] Title: 광고 삽입, Time: 30.38 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.py
@@ -1,3 +1,27 @@
-def solution(play_time, adv_time, logs):
-    answer = ''
-    return answer
+def ttos(x):
+    h, m, s = map(int, x.split(':'))
+    return h*3600 + m*60 + s
+
+
+def solution(play, adv, logs):
+    play = ttos(play)
+    adv = ttos(adv)
+    memo = [0] * (play+1)    
+    for log in logs:
+        s, e = log.split('-')
+        memo[ttos(s)] += 1
+        memo[ttos(e)] -= 1
+    
+    for i in range(play):
+        memo[i+1] += memo[i]
+    
+    cur = sum(memo[:adv])
+    maxy = (0, cur)
+    for i in range(play-adv):
+        cur += memo[i+adv] - memo[i]
+        if maxy[1] < cur:
+            maxy = (i+1, cur)
+    
+    h, maxy = divmod(maxy[0], 3600)
+    m, s = divmod(maxy, 60)
+    return ':'.join(map(lambda x: str(x).rjust(2, '0'), (h, m, s)))
