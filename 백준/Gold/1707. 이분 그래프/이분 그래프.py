6b6006f22b3c9372ce3735589dde71f133b24d58
Seunghoon Yang <hyuio0228@gmail.com>
2023-06-28 12:09:44 +0900

    [Gold IV] Title: 이분 그래프, Time: 1200 ms, Memory: 106828 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,51 +1,29 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
-
-def sol(n: int) -> str:
-    return '\n'.join(is_bipartite(*map(int, input().split())) for _ in range(n))
+sys.setrecursionlimit(pow(10, 9))
 
 
-def is_bipartite(v: int, e: int) -> str:
+def is_bipartite(v, e):
+    def dfs(v, b):
+        memo[v] = b
+        for cv in graph[v]:
+            if memo[cv] == b^1:
+                continue
+            if memo[cv] == b or not dfs(cv, b^1):
+                return False
+        return True
+    
+    
+    memo = [-1] * (v+1)
     graph = [[] for _ in range(v+1)]
     for _ in range(e):
         v1, v2 = map(int, input().split())
         graph[v1].append(v2)
         graph[v2].append(v1)
-
-    is_odd = False
-    v1, v2 = [0]*(v+1), [0]*(v+1)
-    to_visit = set(range(1, v+1))
-    q = deque()
-
-    while to_visit:
-        add_q = to_visit.pop()
-        q.append(add_q)
-        if not is_odd:
-            v1[add_q] = 1
-        else:
-            v2[add_q] = 1
-        
-        while q:
-            for _ in range(len(q)):
-                vertex = q.popleft()
-                for node in graph[vertex]:
-                    if is_odd:
-                        if v2[node]:
-                            return 'NO'
-                        elif not v1[node]:
-                            v1[node] = 1
-                            to_visit.remove(node)
-                            q.append(node)
-                    else:
-                        if v1[node]:
-                            return 'NO'
-                        elif not v2[node]:
-                            v2[node] = 1
-                            to_visit.remove(node)
-                            q.append(node)
-            is_odd = not is_odd
-    return 'YES'
+    answer = all(dfs(i, 0) for i in range(1, v+1) if memo[i] == -1)
+    return 'YES' if answer else 'NO'
 
 
-print(sol(int(input())))
+if __name__ == '__main__':
+    print('\n'.join(is_bipartite(*map(int, input().split())) for _ in range(int(input()))))
+    
