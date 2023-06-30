5e84cbd9c27820f4e4837bced3ee30ca068666fb
Seunghoon Yang <hyuio0228@gmail.com>
2023-06-30 20:18:25 +0900

    [Gold IV] Title: 전화번호 목록, Time: 428 ms, Memory: 42516 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -3,15 +3,20 @@ input = sys.stdin.readline
 
 
 def is_consistent(n):
-    nums = sorted(input().rstrip() for _ in range(n))
     memo = {}
+    nums = [input().rstrip() for _ in range(n)]
     for num in nums:
+        res = False
         cur = memo
         for d in num:
             if '-1' in cur:
                 return 'NO'
-            cur[d] = cur.get(d, {})
+            if d not in cur:
+                res = True
+                cur[d] = {}
             cur = cur[d]
+        if not res:
+            return 'NO'
         cur['-1'] = {}
     return 'YES'
 
