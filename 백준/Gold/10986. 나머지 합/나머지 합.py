78c492a2c0f45a20abd25cd090a0706054e554d1
Seunghoon Yang <hyuio0228@gmail.com>
2023-07-12 14:00:37 +0900

    [Gold III] Title: 나머지 합, Time: 464 ms, Memory: 152772 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,19 +1,20 @@
-# prefix sum + comb (math)
-import sys
-input = sys.stdin.readline
-from math import comb
-
-
-def sol(n: int, m: int) -> int:
-    occur = [0] * m
-    occur[0] = 1
-
-    acc = 0
-    for num in map(int, input().split()):
-        acc = (acc+num) % m
-        occur[acc] += 1
+class ResSum():
+    def __init__(self):
+        self.N, self.M = map(int, input().split())
+        self.arr = list(map(int, input().split()))
+        self.get_num_of_intervals()
+        
     
-    return sum(comb(i, 2) for i in occur if i > 1)
-
-
-print(sol(*map(int, input().split())))
+    def get_num_of_intervals(self):
+        memo = [0] * self.M
+        acc = 0
+        for i in self.arr:
+            acc = (acc + i) % self.M
+            memo[acc] += 1
+        cnt = memo[0] + sum(map(lambda i: i*(i-1)//2, memo))
+        self.cnt = cnt
+        
+    
+if __name__ == '__main__':
+    answer = ResSum()
+    print(answer.cnt)
