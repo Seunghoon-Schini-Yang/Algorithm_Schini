950501e69d32287c0d8a70c70e82ec8451e784ca
howbeen <hyuio0228@gmail.com>
2023-07-26 14:20:56 +0900

    [Gold IV] Title: 약수의 합, Time: 248 ms, Memory: 128688 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.py
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -7,7 +7,6 @@ class Divisor():
         arr = [int(input()) for _ in range(T)]
         maxy = max(arr)+1
         acc = [i+1 for i in range(maxy+1)]
-        acc[0] = 0
         acc[1] -= 1
         for s in range(2, (maxy>>1)+1):
             for d in range(s<<1, maxy+1, s):
