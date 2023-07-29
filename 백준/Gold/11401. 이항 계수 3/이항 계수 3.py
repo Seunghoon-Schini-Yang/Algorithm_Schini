7d3fe39e90eb724ed2cc762abdfdf5cb648f2bd6
howbeen <hyuio0228@gmail.com>
2023-07-29 14:35:21 +0900

    [Gold I] Title: 이항 계수 3, Time: 644 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
@@ -1,18 +1,17 @@
+def factorial(s, e, val, mod):
+    for i in range(s, e):
+        val = (val * i) % mod
+    return val
+
+
 class BiCoeffcient():
     def __init__(self, N, K):
         K = min(N, N-K)
         mod = 1_000_000_007
 
-        cur = 1
-        for i in range(1, K+1):
-            cur = (cur * i) % mod
-        fac_k = cur
-        for i in range(K+1, N-K+1):
-            cur = (cur * i) % mod
-        fac_nk = cur
-        for i in range(N-K+1, N+1):
-            cur = (cur * i) % mod
-        fac_n = cur
+        fac_k = factorial(1, K+1, 1, mod)
+        fac_nk = factorial(K+1, N-K+1, fac_k, mod)
+        fac_n = factorial(N-K+1, N+1, fac_nk, mod)
         
         mul = (fac_k * fac_nk) % mod
         cur = 1
