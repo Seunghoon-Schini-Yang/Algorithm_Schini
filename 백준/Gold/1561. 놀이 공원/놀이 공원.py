3f0f8c7cd36e84873051d87fe7688a1e2f184025
howbeen <hyuio0228@gmail.com>
2023-08-05 15:57:38 +0900

    [Gold II] Title: 놀이 공원, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.py
@@ -0,0 +1,25 @@
+class Luna():
+    def __init__(self, N, M, arr):
+        if N <= M:
+            self.answer = N
+        else:
+            s, e = 0, 60_000_000_000
+            while s < e:
+                m = (s+e)>>1
+                cur = sum(m//v for v in arr) + M
+                if  cur < N:
+                    s = m+1
+                else:
+                    e = m
+            N -= sum((s-1)//v for v in arr) + M
+            for i in range(M):
+                if s % arr[i] == 0:
+                    N -= 1
+                    if N == 0:
+                        self.answer = i+1
+                        break
+
+                
+if __name__ == '__main__':
+    cars = Luna(*map(int, input().split()), list(map(int, input().split())))
+    print(cars.answer)
