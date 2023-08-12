9f771957bcc1fba7c5d5325e82797346020fcec3
howbeen <hyuio0228@gmail.com>
2023-08-12 15:10:14 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5528 ms, Memory: 642276 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,22 +1,25 @@
+# pen2402 님 코드 참고
 import sys
 input = sys.stdin.readline
-from heapq import heappop, heappush
+from collections import deque
 
 
 class Arr():
     def __init__(self):
         self.N, self.L = map(int, input().split())
-        self.arr = map(int, input().split())
-        self.pq = []
-        self.mins = [self._get_min(i, x) for i, x in enumerate(self.arr)]
+        self.arr = tuple(map(int, input().split()))
+        self.dq = deque()
+        self.mins = [self._get_min(i) for i in range(self.N)]
     
     
-    def _get_min(self, i, x):
-        heappush(self.pq, (x, i))
-        while self.pq[0][1] <= i-self.L:
-            heappop(self.pq)
-        return self.pq[0][0]
-        
+    def _get_min(self, i):
+        while self.dq and self.arr[i] < self.dq[-1]:
+            self.dq.pop()
+        self.dq.append(self.arr[i])
+        if self.L <= i and self.arr[i-self.L] == self.dq[0]:
+            self.dq.popleft()
+        return self.dq[0]
+
 
 if __name__ == '__main__':
     arr = Arr()
