6f7ce345d72fad896fca83010320673635bd9b70
howbeen <hyuio0228@gmail.com>
2023-08-25 14:30:03 +0900

    [Gold III] Title: 집으로, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1069. 집으로/집으로.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    X, Y, D, T = map(int, input().split())
+    cur = pow(X**2 + Y**2, 0.5)
+    D2 = D<<1
+    ts = 0
+    if D > T:
+        if D2 < cur:
+            step = ((cur - 2*D) // D) + 1
+            cur -= step*D
+            ts += step*T
+        i = 0 if cur <= D else 1
+        cur = min(
+            ts + T*2,
+            ts + (i+1)*T + ((i+1)*D - cur),
+            ts + i*T + (cur - i*D)
+        )
+    print(cur)
