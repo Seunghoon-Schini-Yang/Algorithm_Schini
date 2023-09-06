ae3cb3e6ffd8be9988114f5ecfa831f475ad7f67
howbeen <hyuio0228@gmail.com>
2023-09-07 01:25:23 +0900

    [Gold I] Title: 비드맨, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/19590. 비드맨/비드맨.py
+++ 백준/Gold/19590. 비드맨/비드맨.py
@@ -4,9 +4,11 @@ input = sys.stdin.readline
 
 class Beads():
     def __init__(self, N):
-        arr = [int(input()) for _ in range(N)]
-        maxy = max(arr)
-        sumy = sum(arr)
+        maxy = sumy = 0
+        for _ in range(N):
+            x = int(input())
+            sumy += x
+            maxy = x if maxy < x else maxy
         sumy -= maxy
         self.res = maxy-sumy if sumy <= maxy else (1 if (sumy-maxy)&1 else 0)
 
