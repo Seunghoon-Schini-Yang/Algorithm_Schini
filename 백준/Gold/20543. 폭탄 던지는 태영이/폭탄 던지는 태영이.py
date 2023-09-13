a17c9ccd613e42622b681e70ccc87d7fc3ba1193
howbeen <hyuio0228@gmail.com>
2023-09-13 12:43:58 +0900

    [Gold I] Title: 폭탄 던지는 태영이, Time: 2636 ms, Memory: 154248 KB -BaekjoonHub

---
+++ 백준/Gold/20543. 폭탄 던지는 태영이/폭탄 던지는 태영이.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+class Inha():
+    def __init__(self, N, M):
+        board = [[0] * N for _ in range(M-1)] + [list(map(lambda x: -int(x), input().split())) for _ in range(N-M+1)]
+        for c in range(N):
+            acc = 0
+            for r in range(M-1, N):
+                board[r][c] -= acc
+                acc += board[r][c] - board[r-M+1][c]
+        board = board[M-1:]
+
+        board = [[0]*(M-1) + row[:(N-M+1)] for row in board]
+        for r in range(N-M+1):
+            acc = 0
+            for c in range(M-1, N):
+                board[r][c] -= acc
+                acc += board[r][c] - board[r][c-M+1]
+        board = [row[M-1:] for row in board]
+        board = [[0]*N for _ in range(M>>1)] + [[0]*(M>>1) + row + [0]*(M>>1) for row in board] + [[0]*N for _ in range(M>>1)]
+        self.board = board
+
+
+if __name__ == '__main__':
+    uni = Inha(*map(int, input().split()))
+    print('\n'.join(' '.join(map(str, row)) for row in uni.board))
